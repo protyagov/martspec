c8f6abf72c7390ea3bb10dc3a31dc564012ddcaf
Alex Protyagov <protyagov@gmail.com>
2022-05-09 02:40:43 -0500

    half finsished emotion translation

--- src/landing/p7-emotion.tsx
+++ src/landing/p7-emotion.tsx
@@ -12,48 +12,38 @@ export default function P7Emotion() {
             <section className="rr-s-about ms-s-offset">
                 <div className="row" >
                     <div className="col-sm-12 col-md-9 order-last order-md-1">
-                        <h3 className="mt-4">{_("PRODUCT.P6.ABOUT")}</h3>
+                        <h3 className="mt-4">{_("PRODUCT.P7.ABOUT_1")}</h3>
+                        <h3 className="mt-4">{_("PRODUCT.P7.ABOUT_2")}</h3>
+                        <h3 className="mt-4">{_("PRODUCT.P7.ABOUT_3")}</h3>
 
                         {/* Body compostion */}
                         <h1>{_("PRODUCT.P7.HEAD1")}</h1>
-                        <h3 className="mt-3">{_("PRODUCT.P6.DESC1")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC1_1")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC1_2")}</h3>
 
 
                         {/* Chest and Bust */}
                         <h1>{_("PRODUCT.P7.HEAD2")}</h1>
-                        <h3 className="mt-3">{_("PRODUCT.P6.DESC2")}</h3>
-
-
-                        {/* Waist Circumferance */}
-                        <h1>{_("PRODUCT.P7.HEAD3")}</h1>
-                        <h3 className="mt-3">
-                            <a href="https://www.heart.org/en/news/2019/03/19/waist-size-predicts-heart-attacks-better-than-bmi-especially-in-women" target="_blank">
-                                {_("PRODUCT.P7.LINK2")}
-                            </a>
-                            {_("PRODUCT.P7.DESC2")}
-                        </h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC2_1")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC2_2")}</h3>
                         
+                        <h1>{_("PRODUCT.P7.HEAD3")}</h1>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC3_1")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC3_2")}</h3>
 
                         {/* Hips Width */}
                         <h1>{_("PRODUCT.P7.HEAD4")}</h1>
                         <h3 className="mt-3">{_("PRODUCT.P7.DESC4")}</h3>
 
 
-                        <h1>{_("PRODUCT.P7.HEAD1")}</h1>
-                        <h3 className="mt-3">{_("PRODUCT.P7.DESC1")}
-                            <a href="https://www.nhlbi.nih.gov/health/educational/lose_wt/risk.htm" target="_blank">
-                                <br /><small><i>{_("PRODUCT.P7.LINK1")}</i></small>
-                            </a>
-                        </h3>
-
-
                         {/* Body Shape */}
                         <h1>{_("PRODUCT.P7.HEAD5")}</h1>
-                        <h3 className="mt-3">{_("PRODUCT.P7.DESC5")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC5_1")}</h3>
+                        <h3 className="mt-3">{_("PRODUCT.P7.DESC5_2")}</h3>
                     </div>
 
                     <div className="col-sm-12 col-md-3 order-sm-1">
-                        <img src="/img/bodysize.webp" className="mx-auto" alt={_("MAIN.P6.IMG")} />
+                        <img src="/img/p_emotion_face.webp" className="mx-auto" alt={_("MAIN.P6.IMG")} />
                     </div>
                 </div>
             </section>
@@ -62,7 +52,7 @@ export default function P7Emotion() {
             <div className="container mb-5 pb-5">
                 <div className="row align-items-center justify-content-sm-center" >
                     <div className="col-md-7 col-12 pb-5">
-                        <IronImage imgLo="/img/p_bodysize_screens_sm.webp" imgHi="/img/p_bodysize_screens.webp" text={_("MAIN.P6.IMG")} />
+                        <IronImage imgLo="/img/p_emotion_screens_sm.webp" imgHi="/img/p_emotion_screens.webp" text={_("MAIN.P6.IMG")} />
                     </div>
                     <div className="col-md-5 col-12 text-center pb-5">
                         <a href={`https://apps.apple.com/${_("APPSTORE")}/app/id1562956213`}
