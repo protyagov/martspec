8142925758b779ca3fcf2f9a888fdc97e6276391
Alex Protyagov <protyagov@gmail.com>
2022-08-10 00:16:44 -0500

    Update about page

--- src/landing/about.tsx
+++ src/landing/about.tsx
@@ -17,6 +17,7 @@ export default function About() {
                 <p>{_("ABOUT.P4")}</p>
                 <p>{_("ABOUT.P5")}</p>
                 <p>{_("ABOUT.P6")}</p>
+                <p>{_("ABOUT.P7")}</p>
             </section>
         </div>
         <Footer />
