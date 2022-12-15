908f961793d0cc7a393423c09614ba381fe34eeb
Nichita Trifan <nikiton254@gmail.com>
2022-12-14 20:25:53 -0600

    setting border-radius for small navbar through states

--- src/part/navbar.tsx
+++ src/part/navbar.tsx
@@ -2,15 +2,22 @@ import * as React from "react";
 import _, { Locale } from "src/i18n/locale"
 
 export default function NavigationBar() {
+    const [navShow, setNavShow] = React.useState(false);
+
     return (
-        <nav className="ms-navbar-nav navbar-expand-lg">
+        <nav className="ms-navbar-nav navbar-expand-lg"
+            style={{
+                borderRadius: navShow ? "0px 0px 16px 16px": "0px 0px 0px 0px"
+            }
+        }>
             <div className="container-fluid">
 
                 <a className="navbar-brand"  href={Locale.i18nLink("/")}>
                     <img src="/img/logo.64.webp" alt={_("ERROR.404_BTN")} width={64} height={64}/>
                 </a>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                    onClick={() => setNavShow(!navShow)}>
                     <span className="navbar-toggler-icon">
                         <img src="/img/burger_menu.svg" alt={_("ERROR.404_BTN")} width={32} height={32}/>
                     </span>
