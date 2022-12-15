969bf4a1fd3ba4cd4f74f1cf021805c693eed1ef
protyagov <protyagov@gmail.com>
2022-12-15 10:38:46 -0600

    Update section for vitamin

--- src/landing/main.tsx
+++ src/landing/main.tsx
@@ -21,14 +21,14 @@ export default function LandingPage() {
             <section className="rr-s-product pw-color-bg-secondary">
                 <div className="row ms-s-offset">
                     <div className="col-md-6 align-self-center order-last order-md-1">
+                        <h1><img src="/img/vitamin_logo.svg" className="img-fluid mb-2 me-3" width={48} /> {_("MAIN.P3.HEAD")}</h1>
                         <p className="mt-2">{_("MAIN.P3.SUBHEAD")}</p>
-                        <h1>{_("MAIN.P3.HEAD")}</h1>
                         <h2>{_("MAIN.P3.TEXT")}</h2>
                         <a href={Locale.i18nLink("logvitamin")} className="mt-3 rr-btn-large">{_("MAIN.P3.BTN")}</a>
                     </div>
 
                     <div className="col-md-4 order-sm-1">
-                        <IronImage imgLo="/img/p_vitamin.64.webp" imgHi="/img/p_vitamin.512.webp" text={_("MAIN.P3.IMG")} h={540} w={540} />
+                        <IronImage imgLo="/img/vitamin.64.webp" imgHi="/img/vitamin.512.webp" i18n={true} text={_("MAIN.P3.IMG")} h={540} w={540} />
                     </div>
                 </div>
             </section>
