b1591c6e4e3ebb4f605b6d990060d63dcca5d7eb
protyagov <protyagov@gmail.com>
2022-12-17 02:19:51 -0600

    nav bar using standard bootstrap 5.2 components

--- src/part/navbar.tsx
+++ src/part/navbar.tsx
@@ -1,104 +1,99 @@
 import * as React from "react";
 import _, { Locale } from "src/i18n/locale"
 
+function NavigationBarLanguageDropdown() {
+    return <ul className="navbar-nav">
+
+        <li className="nav-item dropdown">
+            <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="/img/language_dropdown.svg" alt={_("ERROR.404_BTN")} width={32} height={32} />
+            </a>
+            <ul className="dropdown-menu dropdown-position border-1">
+                <li className="dropdown-item" hidden={Locale.language == 'en'}>
+                    <a className="nav-link" href="/en">English</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'ru'}>
+                    <a className="nav-link" href="/ru">Русский</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'de'}>
+                    <a className="nav-link" href="/de">Deutsche</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'fr'}>
+                    <a className="nav-link" href="/fr">Français</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'hi'}>
+                    <a className="nav-link" href="/hi">हिन्दी</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'pt'}>
+                    <a className="nav-link" href="/pt">Português</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'es'}>
+                    <a className="nav-link" href="/es">Español</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'zh'}>
+                    <a className="nav-link" href="/zh">中文</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'ja'}>
+                    <a className="nav-link" href="/ja">日本語</a>
+                </li>
+                <li className="dropdown-item" hidden={Locale.language == 'ar'}>
+                    <a className="nav-link" href="/ar">عربي</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+}
+
+
 export default function NavigationBar() {
-    const [navShow, setNavShow] = React.useState(false);
+    return <nav className="ms-navbar navbar-expand-lg px-lg-5">
+        <div className="container-fluid">
 
-    return (
-        <nav className="ms-navbar-nav navbar-expand-lg"
-            style={{
-                borderRadius: navShow ? "0px 0px 16px 16px": "0px 0px 0px 0px"
-            }
-        }>
-            <div className="container-fluid">
+            <a className="navbar-brand" href={Locale.i18nLink("/")}>
+                <img src="/img/logo.64.webp" alt={_("ERROR.404_BTN")} className="d-inline-block align-text-top"
+                    width={64} height={48} />
+            </a>
 
-                <a className="navbar-brand"  href={Locale.i18nLink("/")}>
-                    <img src="/img/logo.64.webp" alt={_("ERROR.404_BTN")} width={64} height={64}/>
-                </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+            </button>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-                    onClick={() => setNavShow(!navShow)}>
-                    <span className="navbar-toggler-icon">
-                        <img src="/img/burger_menu.svg" alt={_("ERROR.404_BTN")} width={32} height={32}/>
-                    </span>
-                </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav me-auto">
 
-                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                    <ul className="navbar-nav">
+                    <li className="nav-item dropdown ps-lg-5">
+                        <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {_("HEADER.ABOUT.TITLE")}
+                        </a>
 
-                        <li className={
-                            navShow ? "nav-item dropend": "nav-item dropdown"
-                        }>
-                            <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {_("HEADER.ABOUT.TITLE")}
-                            </a>
-
-                            <ul className="dropdown-menu dropdown-position border-1"
+                        <ul className="dropdown-menu dropdown-position border-1"
                             aria-labelledby="navbarDropdown">
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI1")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI2")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI1")}</a></li>
-                            </ul>
-                        </li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI1")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI2")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.ABOUT.LI1")}</a></li>
+                        </ul>
+                    </li>
 
-                        <li className="nav-item dropdown">
-                            <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {_("HEADER.APPLICATION.TITLE")}
-                            </a>
-                            <ul className="dropdown-menu dropdown-position border-1" aria-labelledby="navbarDropdown">
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI1")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI2")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI3")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI4")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI5")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI6")}</a></li>
-                                <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI7")}</a></li>
-                            </ul>
-                        </li>
 
-                        <li className="nav-item dropdown">
-
-                                <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="/img/language_dropdown.svg" alt={_("ERROR.404_BTN")} width={32} height={32}/>
-                                </a>
-                                <ul className="dropdown-menu dropdown-position border-1">
-                                    <li className="dropdown-item" hidden={Locale.language == 'en'}>
-                                        <a className="nav-link" href="/en">English</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'ru'}>
-                                        <a className="nav-link" href="/ru">Русский</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'de'}>
-                                        <a className="nav-link" href="/de">Deutsche</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'fr'}>
-                                        <a className="nav-link" href="/fr">Français</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'hi'}>
-                                        <a className="nav-link" href="/hi">हिन्दी</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'pt'}>
-                                        <a className="nav-link" href="/pt">Português</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'es'}>
-                                        <a className="nav-link" href="/es">Español</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'zh'}>
-                                        <a className="nav-link" href="/zh">中文</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'ja'}>
-                                        <a className="nav-link" href="/ja">日本語</a>
-                                    </li>
-                                    <li className="dropdown-item" hidden={Locale.language == 'ar'}>
-                                        <a className="nav-link" href="/ar">عربي</a>
-                                    </li>
-                                </ul>
-
-                        </li>
-                    </ul>
-
-                </div>
+                    <li className="nav-item dropdown ps-lg-5">
+                        <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {_("HEADER.APPLICATION.TITLE")}
+                        </a>
+                        <ul className="dropdown-menu dropdown-position border-1" aria-labelledby="navbarDropdown">
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI1")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI2")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI3")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI4")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI5")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI6")}</a></li>
+                            <li><a className="nav-link dropdown-item" href="#">{_("HEADER.APPLICATION.LI7")}</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                {NavigationBarLanguageDropdown()}
             </div>
-        </nav>
-    )
+        </div>
+    </nav>
 }
