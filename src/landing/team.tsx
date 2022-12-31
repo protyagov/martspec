9026c61f51478716b12246a3f120c3b66bdc6af0
Alevtina <stochna.alevtina@gmail.com>
2022-12-31 18:32:53 +0200

    [MS-1] team page add & setup

---
+++ src/landing/team.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Footer } from "src/part/footer";
+import NavigationBar from "src/part/navbar";
+
+export default function Team() {
+    return <>
+        <NavigationBar />
+
+        <div className="ms-base-page pb-5">
+        </div>
+          
+        <Footer />
+    </>
+}
