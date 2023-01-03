9d1c3f40ebf82edc39dbc16092b823f2ff85f6f8
Alevtina <stochna.alevtina@gmail.com>
2023-01-03 11:11:29 +0200

    [MS-1] added img placeholder for TeamMember

--- src/part/teamMember.tsx
+++ src/part/teamMember.tsx
@@ -9,8 +9,9 @@ interface TeamMemberProps {
 
 export default function TeamMember(props: TeamMemberProps) {
     return <div className="mx-4 d-flex flex-column text-center">
-        <img src={props.avatar} alt={_("TEAM." + props.title) + " " + props.name}
-          height="138" width="138" className="rounded-circle mb-4" />
+        <img src={props.avatar} onError={e => e.currentTarget.src="/img/team_placeholder.png"}
+            alt={_("TEAM." + props.title) + " " + props.name}
+            height="138" width="138" className="rounded-circle mb-4" />
         <span className="fs-4">
             { props.name }
         </span>
