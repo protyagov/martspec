5abea5fdef634fa15ac81797fdf8c44ee8cd2b6f
Alevtina <stochna.alevtina@gmail.com>
2023-01-03 10:09:59 +0200

    [MS-1] added TeamMember component

---
+++ src/part/teamMember.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import _ from "src/i18n/locale";
+
+interface TeamMemberProps {
+    name: string;
+    title: string;
+    avatar: string;
+}
+
+export default function TeamMember(props: TeamMemberProps) {
+    return <div className="mx-4 d-flex flex-column text-center">
+        <img src={props.avatar} alt={_("TEAM." + props.title) + " " + props.name}
+          height="138" width="138" className="rounded-circle mb-4" />
+        <span className="fs-4">
+            { props.name }
+        </span>
+        <span className="fs-4 fw-light">
+            { _("TEAM." + props.title) }
+        </span>
+    </div>
+}
