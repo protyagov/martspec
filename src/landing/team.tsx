9afe0d595bdc9863f7f701be7f3f07e91e955847
Alevtina <stochna.alevtina@gmail.com>
2023-01-03 09:32:49 +0200

    [MS-1] added state and fetching function to Team component

--- src/landing/team.tsx
+++ src/landing/team.tsx
@@ -3,6 +3,17 @@ import { Footer } from "src/part/footer";
 import NavigationBar from "src/part/navbar";
 
 export default function Team() {
+    const [teamData, setTeamData] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchTeamData = async (): Promise<void> => {
+            const response = await fetch("/data/team.json");
+            if (!response.ok) return;
+            setTeamData(await response.json());
+        };
+        fetchTeamData();
+    }, []);
+
     return <>
         <NavigationBar />
 
