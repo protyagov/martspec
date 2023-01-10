6d9fa0804068ff1e41d186d9a8cbd45d41286a0b
Alevtina <stochna.alevtina@gmail.com>
2023-01-10 12:47:26 +0200

    updated main section layout

--- src/landing/main.tsx
+++ src/landing/main.tsx
@@ -69,15 +69,22 @@ export default function LandingPage() {
 
 
             <section className="rr-s-product pw-color-bg-secondary">
-                <div className="row ms-s-offset">
+                <div className="row ms-s-offset align-items-center">
                     <div className="col-md-6 align-self-center order-last order-md-1">
+                        <h1 className="d-none d-md-block">
+                            <img src="/img/logo_electrolyte.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("ELECTROLYTE.HEAD")}
+                        </h1>
                         <p className="mt-2">{_("ELECTROLYTE.SUBHEAD")}</p>
-                        <h1>{_("ELECTROLYTE.HEAD")}</h1>
                         <h2>{_("ELECTROLYTE.TEXT")}</h2>
                         <a href={Locale.i18nLink("electrolyte")} className="mt-3 rr-btn-large">{_("ELECTROLYTE.BTN")}</a>
                     </div>
                     <div className="col-md-4 order-sm-1">
-                        <IronImage imgLo="/img/p_electrolyte.64.webp" imgHi="/img/p_electrolyte.512.webp" text={_("ELECTROLYTE.IMG")} h={540} w={540} />
+                        <h1 className="d-block d-md-none">
+                            <img src="/img/logo_electrolyte.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("ELECTROLYTE.HEAD")}
+                        </h1>
+                        <ImageI18N src="/img/p_electrolyte.512.webp" w={512} h={512} cls="mx-auto img-fluid" alt={_("ELECTROLYTE.IMG")} />
                     </div>
                 </div>
             </section>
@@ -110,27 +117,40 @@ export default function LandingPage() {
             <section className="rr-s-product pw-color-bg-secondary">
                 <div className="row ms-s-offset">
                     <div className="col-md-6 align-self-center order-last order-md-1">
+                        <h1 className="d-none d-md-block">
+                            <img src="/img/logo_zinc.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("ZINC.HEAD")}
+                        </h1>
                         <p className="mt-2">{_("ZINC.SUBHEAD")}</p>
-                        <h1>{_("ZINC.HEAD")}</h1>
                         <h2>{_("ZINC.TEXT")}</h2>
                         <a href={Locale.i18nLink("bodyzinc")} className="mt-3 rr-btn-large">{_("ZINC.BTN")}</a>
                     </div>
-
                     <div className="col-md-4 order-sm-1">
-                        <IronImage imgLo="/img/p_zinc.64.webp" imgHi="/img/p_zinc.512.webp" text={_("ZINC.IMG")} h={540} w={540} />
+                        <h1 className="d-block d-md-none">
+                            <img src="/img/logo_zinc.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("ZINC.HEAD")}
+                        </h1>
+                        <ImageI18N src="/img/p_zinc.512.webp" w={512} h={512} cls="mx-auto img-fluid" alt={_("ZINC.IMG")} />
                     </div>
                 </div>
             </section>
 
             <section className="rr-s-product my-5">
                 <div className="row ms-s-offset">
-                    <div className="col-md-4">
-                        <IronImage imgLo="/img/p_waist.64.webp" imgHi="/img/p_waist.512.webp" text={_("WAIST.IMG")} h={540} w={540} />
+                    <div className="col-md-4 order-sm-1">
+                        <h1 className="d-block d-md-none">
+                            <img src="/img/logo_waist.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("WAIST.HEAD")}
+                        </h1>
+                        <ImageI18N src="/img/p_waist.512.webp" w={512} h={512} cls="mx-auto img-fluid" alt={_("WAIST.IMG")} />                        
                     </div>
 
-                    <div className="col-md-6 align-self-center">
+                    <div className="col-md-6 align-self-center order-last order-md-1">
+                        <h1 className="d-none d-md-block">
+                            <img src="/img/logo_waist.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("WAIST.HEAD")}
+                        </h1>
                         <p className="mt-2">{_("WAIST.SUBHEAD")}</p>
-                        <h1>{_("WAIST.HEAD")}</h1>
                         <h2>{_("WAIST.TEXT")}</h2>
                         <a href={Locale.i18nLink("waistline")} className="mt-3 rr-btn-large pull-right">{_("WAIST.BTN")}</a>
                     </div>
@@ -141,14 +161,20 @@ export default function LandingPage() {
             <section className="rr-s-product pw-color-bg-secondary">
                 <div className="row ms-s-offset">
                     <div className="col-md-6 align-self-center order-last order-md-1">
+                        <h1 className="d-none d-md-block">
+                            <img src="/img/logo_emotion.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("EMOTION.HEAD")}
+                        </h1>
                         <p className="mt-2">{_("EMOTION.SUBHEAD")}</p>
-                        <h1>{_("EMOTION.HEAD")}</h1>
                         <h2>{_("EMOTION.TEXT")}</h2>
                         <a href={Locale.i18nLink("emotion")} className="mt-3 rr-btn-large">{_("EMOTION.BTN")}</a>
                     </div>
-
                     <div className="col-md-4 order-sm-1">
-                        <IronImage imgLo="/img/p_emotion.64.webp" imgHi="/img/p_emotion.512.webp" text={_("EMOTION.IMG")} h={540} w={540} />
+                        <h1 className="d-block d-md-none">
+                            <img src="/img/logo_emotion.svg" className="img-fluid mb-2 me-3" width={48} />
+                            {_("EMOTION.HEAD")}
+                        </h1>
+                        <ImageI18N src="/img/p_emotion.512.webp" w={512} h={512} cls="mx-auto img-fluid" alt={_("EMOTION.IMG")} />
                     </div>
                 </div>
             </section>
