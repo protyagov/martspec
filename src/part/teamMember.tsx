4949e695901f8a5c392f70f08cdc1aaa5e87c01e
Alevtina <stochna.alevtina@gmail.com>
2023-04-19 17:51:23 +0300

    [MS-261] deleted set in html img height & width

--- src/part/teamMember.tsx
+++ src/part/teamMember.tsx
@@ -11,7 +11,7 @@ export default function TeamMember(props: TeamMemberProps) {
     return <div className="d-flex flex-column team-member">
         <img src={props.avatar} onError={e => e.currentTarget.src="/img/team_placeholder.png"}
             alt={_("TEAM." + props.title) + " " + props.name}
-            height="150" width="150" className="rounded-circle mb-2 p-0" />
+            className="rounded-circle mb-2 p-0" />
         <h4> { _("TEAM." + props.name) } </h4>
         <h5> { _("TEAM." + props.title) } </h5>
     </div>
