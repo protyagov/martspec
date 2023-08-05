4e53bc8cbb0e24a8ec4830329c9f929f9d98e648
Alevtina <stochna.alevtina@gmail.com>
2023-08-05 16:22:08 +0300

    [MS-260] updated nav offset

--- src/part/navbar.tsx
+++ src/part/navbar.tsx
@@ -38,8 +38,8 @@ function NavigationBarLanguageDropdown() {
 
 
 export default function NavigationBar() {
-    return <nav className="ms-navbar navbar-expand-lg px-lg-5">
-        <div className="container-fluid">
+    return <nav className="ms-navbar navbar-expand-lg">
+        <div className="ms-s-offset my-0">
 
             <a className="navbar-brand" href={Locale.i18nLink("/")}>
                 <img src="/img/logo.64.webp" alt={_("ERROR.404_BTN")} className="d-inline-block align-text-top"
