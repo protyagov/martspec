d8f89b2891aa27870101b7f306f9cc4be451db6b
Nataliia Semenova <natasemenova1806@gmail.com>
2023-08-14 00:24:51 +0300

    done

--- src/part/footer.tsx
+++ src/part/footer.tsx
@@ -51,19 +51,19 @@ export const Footer = () =>
 
                     <div>
                         <a className="social-icon" href="https://www.linkedin.com/company/martspec" target="_blank" rel="noopener" aria-label="">
-                            <img src="/img/linkedin_icon.svg" alt="linkedin" />
+                            <img src="/img/linkedin_icon.svg" alt="linkedin" width={40} height={40} />
                         </a>
 
                         <a className="social-icon" href={`mailto:support@martspec.com?subject=Martspec Question&body=I have a question about your products. `} target="_self" rel="noopener" aria-label="">
-                            <img src="/img/email_icon.svg" alt="email" />
+                                <img src="/img/email_icon.svg" alt="email" width={40} height={40} />
                         </a>
 
                         <a className="social-icon" href="https://twitter.com/MartspecCom" target="_blank" rel="noopener" aria-label="">
-                            <img src="/img/twitter_icon.svg" alt="twitter" />
+                                <img src="/img/twitter_icon.svg" alt="twitter" width={40} height={40} />
                         </a>
 
                         <a className="social-icon" href="https://www.facebook.com/martspec" target="_blank" rel="noopener" aria-label="">
-                            <img src="/img/facebook_icon.svg" alt="facebook" />
+                                <img src="/img/facebook_icon.svg" alt="facebook" width={40} height={40} />
                         </a>
                     </div>
 
