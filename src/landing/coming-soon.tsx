92a81676caaabc32b4ade2d50f456ff005ad4479
Alevtina <stochna.alevtina@gmail.com>
2023-08-29 16:23:23 +0300

    [MS-760] offsets adjustments

--- src/landing/coming-soon.tsx
+++ src/landing/coming-soon.tsx
@@ -2,16 +2,16 @@ import * as React from "react";
 import _ from "src/i18n/locale"
 
 export default function ComingSoon() {
-    return <div className="ms-base-page text-center pb-5 mt-5">
+    return <div className="ms-base-page text-center py-5">
         <section>
-            <img src="/img/coming-soon.png" className="mb-5" alt={_("COMING_SOON.HEAD")} height={300} width={328} />
-            <h1 className="mb-4">{_("COMING_SOON.HEAD")}</h1>
+            <img src="/img/coming-soon.png" className="mb-4" alt={_("COMING_SOON.HEAD")} height={300} width={328} />
+            <h1 className="mb-3" style={{fontSize: "3.2rem"}}>{_("COMING_SOON.HEAD")}</h1>
             <p className="mb-0">{_("COMING_SOON.TEXT_1")}</p>
-            <p className="mb-5">{_("COMING_SOON.TEXT_2")}</p>
-            <a href="/" className="ms-btn-large">{_("COMING_SOON.BTN")}</a>
+            <p className="mb-4">{_("COMING_SOON.TEXT_2")}</p>
+            <a href="/" className="ms-btn-large mt-1 mb-4">{_("COMING_SOON.BTN")}</a>
         </section>
         <section>
-            <p>{_("COMING_SOON.CTA")}</p>
+            <h4 className="pt-3 mb-1">{_("COMING_SOON.CTA")}</h4>
             <div className="d-flex justify-content-center flex-nowrap">
                 <a className="social-icon" href="https://www.linkedin.com/company/martspec" target="_blank" rel="noopener" aria-label="">
                     <img src="/img/linkedin_icon.svg" alt="linkedin" width={40} height={40} />
