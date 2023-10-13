f6916e3102b39abafead384ff0da007f0be43702
Alevtina <stochna.alevtina@gmail.com>
2023-10-13 19:10:43 +0300

    [MS-788] rename args to suit getElementById method

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,11 +1,11 @@
 import { ScrollSpy } from "bootstrap";
 import React from "react";
 
-export const useScrollSpy = (scrollContainerID: string, scrollTargetID: string) => {
-    const scrollContainer = document.getElementById(scrollContainerID);
+export const useScrollSpy = (scrollContainerId: string, scrollTargetId: string) => {
+    const scrollContainer = document.getElementById(scrollContainerId);
     React.useLayoutEffect(() => {
         const scrollSpy = new ScrollSpy(scrollContainer, {
-            target: '#' + scrollTargetID
+            target: '#' + scrollTargetId
         })
     }, []);
 };
