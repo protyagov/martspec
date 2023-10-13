9ff49e30bc7e5d2ef3e6c5ffb4ece57b094412d5
Alevtina <stochna.alevtina@gmail.com>
2023-10-13 20:32:32 +0300

    [MS-788] added target id validation

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -4,8 +4,7 @@ import React from "react";
 export const useScrollSpy = (scrollTargetId: string, scrollContainerId: string = "root") => {
     const scrollContainer = document.getElementById(scrollContainerId);
     React.useLayoutEffect(() => {
-        const scrollSpy = new ScrollSpy(scrollContainer, {
-            target: '#' + scrollTargetId
-        })
+        const target = scrollTargetId.replace(/^[^#]+/, "#$&");
+        const scrollSpy = new ScrollSpy(scrollContainer, { target })
     }, []);
 };
