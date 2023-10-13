88d3e4c2038df965e2f17e76b73a80e9fc09121c
Alevtina <stochna.alevtina@gmail.com>
2023-10-13 21:37:39 +0300

    [MS-770] updated corresponding router imports

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import MainPage from "src/page/main";
 import Error_404 from 'src/error/404';
-import P4WaistLine from "./page/waist";
-import P1Electrolyte from "./page/electrolyte";
-import P2bodymass from "./page/mass";
+import WaistLine from "./page/waist";
+import Electrolyte from "./page/electrolyte";
+import Bodymass from "./page/mass";
 import Vitamin from "./page/vitamin";
 import VitaminElement from "./page/vitamin-element";
 import PrivacyPolicy from "./page/private-policy";
 import { Locale } from "src/i18n/locale";
-import P5BodyZinc from "./page/bodyzinc";
-import P6BodySize from "./page/bodysize";
-import P7Emotion from "./page/emotion";
+import BodyZinc from "./page/bodyzinc";
+import BodySize from "./page/bodysize";
+import Emotion from "./page/emotion";
 import Productivity from "./page/productivity";
 import Biorhythms from "./page/biorhythms";
 import About from "src/page/about";
@@ -35,23 +35,23 @@ export function App() {
         case "/":
             return <MainPage />;
         case "/electrolyte":
-            return <P1Electrolyte />;
+            return <Electrolyte />;
         case "/bodymass":
-            return <P2bodymass />;
+            return <Bodymass />;
         case "/waistline":
-            return <P4WaistLine />;
+            return <WaistLine />;
         case "/vitamin":
             return <Vitamin />;
         case "/bodyzinc":
-            return <P5BodyZinc />;
+            return <BodyZinc />;
         case "/emotion":
-            return <P7Emotion />;
+            return <Emotion />;
         case "/emotion/productivity":
             return <Productivity />;
         case "/emotion/biorhythms":
             return <Biorhythms />
         case "/bodysize":
-            return <P6BodySize />;
+            return <BodySize />;
         case "/privacy-policy":
             return <PrivacyPolicy />;
         case "/about":
