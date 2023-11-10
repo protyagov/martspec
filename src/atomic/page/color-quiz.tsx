d6701808ff45f0a406839041eaba14fba595541a
Alevtina <stochna.alevtina@gmail.com>
2023-11-10 23:23:55 +0200

    [MS-827] added & styles ColorQuiz top section

--- src/atomic/page/color-quiz.tsx
+++ src/atomic/page/color-quiz.tsx
@@ -8,10 +8,14 @@ export default function ColorQuiz() {
 		<>
 			<NavigationBar />
 
-			<div className="ms-base-page ms-base-new emotion">
-				<section className="sample-test-header text-center my-0">
-					<h2>{_('EMOTION.LIST1.LI1_HEAD')}</h2>
-					<p>{_('EMOTION.LIST1.LI1_TEXT')}</p>
+			<div className="ms-base-page ms-base-new emotion color-quiz">
+				<section className="text-center my-0">
+					<div className="row">
+						<div className="col">
+							<h2>{_("COLOR_QUIZ.HEAD1")}</h2>
+							<p className="mb-0">{_("COLOR_QUIZ.DESC1")}</p>
+						</div>
+					</div>
 				</section>
 			</div>
 		</>
