137b316598012bce2b6d2c0637bc270efcb0f38e
Alevtina <stochna.alevtina@gmail.com>
2023-11-12 16:20:24 +0200

    [MS-827] implemented shuffle

--- src/atomic/page/color-test.tsx
+++ src/atomic/page/color-test.tsx
@@ -3,7 +3,56 @@ import _, { Locale } from 'src/i18n/locale';
 import NavigationBar from 'src/atomic/organism/navbar';
 import ImageI18N from 'src/atomic/atom/img-i18n';
 
-export default function ColorTest() {
+const COLORS = [
+	"98938D",
+	"004983",
+	"1D9772",
+	"F12F23",
+	"F2DD00",
+	"D42481",
+	"C55223",
+	"000000"
+] as const;
+
+export default function ColorTest() {	
+	const [sectorModelCollection, setSectorModelCollection] = React.useState(
+		COLORS.map((color, id) => ({ color, id }))
+	);
+
+	const shuffle = () => {
+		setSectorModelCollection(current => [...current].sort(() => Math.random() - 0.5));
+	};
+
+	const hasWrongOrder = () => {
+		for (let i = 0; i <= 7; i++) {
+			if (isNextTo(i)) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	const isNextTo = (id: number) => {
+		if (!isGBB(id)) {
+			return false;
+		}
+
+		const idxBefore = id === 0 ? 7 : id - 1;
+		const idxAfter = id === 7 ? 0 : id + 1;
+
+		return isGBB(idxBefore) || isGBB(idxAfter);
+	};
+
+	const isGBB = (id: number) => {
+		return sectorModelCollection[id].id === 7 || sectorModelCollection[id].id < 2;
+	};
+
+	React.useLayoutEffect(() => {
+		if (hasWrongOrder()) {
+			shuffle();
+		}
+	}, [sectorModelCollection]);
+
 	return (
 		<>
 			<NavigationBar />
@@ -24,6 +73,18 @@ export default function ColorTest() {
 							<h1>{_("COLOR_TEST.TEST_HEAD")}</h1>
 							<p>{_("COLOR_TEST.TEST_DESC")}</p>
 						</div>
+					</div>
+
+					<div className="row">
+						{
+							sectorModelCollection.map((sector) => (
+								<div
+									key={sector.color}
+									className="col-3"
+									style={{ background: "#" + sector.color, height: "250px" }}
+								></div>
+							))
+						}
 					</div>
 				</section>
 
