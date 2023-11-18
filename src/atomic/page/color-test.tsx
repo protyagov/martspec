1b4074ff3fcdb8d448ecdec0c570380b3921c70c
Alevtina <stochna.alevtina@gmail.com>
2023-11-18 19:52:33 +0200

    [MS-827] implemented useSelected hook

--- src/atomic/page/color-test.tsx
+++ src/atomic/page/color-test.tsx
@@ -24,6 +24,13 @@ type useShuffleArgs = [
 	React.Dispatch<React.SetStateAction<SectorModel[]>>
 ];
 
+type useSelectedArgs = {
+	userColorSelectionIds: number[],
+	setTestResult: React.Dispatch<React.SetStateAction<TestResult>>
+}
+
+type TestResult = [string, Icon[], IconColor];
+
 const useShuffled = ([collection, setCollection]: useShuffleArgs) => {
 
 	const shuffle = () => {
@@ -66,14 +73,79 @@ const useShuffled = ([collection, setCollection]: useShuffleArgs) => {
 	}, [collection]);
 };
 
+const useSelected = ({userColorSelectionIds, setTestResult}: useSelectedArgs) => {
+
+	const monEnergy = (): number => {
+		let iRed = 0, iYel = 0, iBlu = 0, iGrn = 0;
+		const uc = userColorSelectionIds;
+
+		for (let i = 0; i <= 7; i++) {
+			switch (uc[i]) {
+				case 3:
+					iRed = i + 1;
+					break;
+				case 4:
+					iYel = i + 1;
+					break;
+				case 2:
+					iGrn = i + 1;
+					break;
+				case 1:
+					iBlu = i + 1;
+					break;
+				default:
+					continue;
+			}
+		}
+
+		return (18 - iRed - iYel) / (18 - iBlu - iGrn);
+	};
+
+	const energyForValue = (v: number): TestResult => {
+		switch (true) {
+			case v >= -1 && v < 0.5:
+				return ["E1", [v >= 0.2 ? Icon.HALF : Icon.NULL, Icon.NULL, Icon.NULL, Icon.NULL, Icon.NULL], IconColor.RED];
+			case v >= 0.5 && v < 0.9:
+				return ["E2", [Icon.FULL, v >= 0.7 ? Icon.HALF : Icon.NULL, Icon.NULL, Icon.NULL, Icon.NULL], IconColor.YELLOW];
+			case v >= 0.9 && v < 1.3:
+				return ["E3", [Icon.FULL, Icon.FULL, v >= 1.1 ? Icon.HALF : Icon.NULL, Icon.NULL, Icon.NULL], IconColor.GREEN];
+			case v >= 1.3 && v < 1.9:
+				return ["E3", [Icon.FULL, Icon.FULL, Icon.FULL, v >= 1.6 ? Icon.HALF : Icon.NULL, Icon.NULL], IconColor.ORANGE];
+			default:
+				return ["E4", [Icon.FULL, Icon.FULL, Icon.FULL, Icon.FULL, v <= 2.6 ? Icon.HALF : Icon.FULL], IconColor.RED];
+		}
+	};
+
+	React.useLayoutEffect(() => {
+		if (userColorSelectionIds.length !== COLORS.length) return;
+		setTestResult(energyForValue(monEnergy()));
+	}, [userColorSelectionIds]);
+};
+
+const enum Icon {
+	NULL,
+	HALF,
+	FULL
+};
+
+const enum IconColor {
+	RED = "FF392E",
+	ORANGE = "FE8429",
+	YELLOW = "E0BD64",
+	GREEN = "A8E03B",
+}
+
 export default function ColorTest() {
 	const sectorModelCollection = React.useState(
 		COLORS.map((color, id) => ({ color, id }))
 	);
 
 	const [userColorSelectionIds, setUserColorSelectionIds] = React.useState([]);
+	const [testResult, setTestResult] = React.useState<TestResult>(null);
 
 	useShuffled(sectorModelCollection);
+
+	useSelected({userColorSelectionIds, setTestResult});
 
 	const sectorClickHandler = (id: number) => setUserColorSelectionIds(current => [...current, id]);
 
@@ -99,8 +171,14 @@ export default function ColorTest() {
 						</div>
 					</div>
 
-					<div className="row flex-wrap justify-content-center g-4 color-sectors">
-						{sectorModelCollection[0].map((sector) => (
+					<div className="row flex-wrap justify-content-center g-4 text-start color-sectors">
+						{testResult ?
+							<div className="col-12 d-flex flex-column">
+								<h2>{_("COLOR_TEST.RES")}</h2>
+								<p>{_("COLOR_TEST." + testResult[0])}</p>
+							</div>
+							:
+							sectorModelCollection[0].map((sector) => (
 							<div key={sector.color} className="col-lg-3 col-6">
 								<button
 									onClick={() => sectorClickHandler(sector.id)}
