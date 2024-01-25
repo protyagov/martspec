a22d8d3666349c46f0e7a50ca888a1e47fce760e
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-01-25 22:45:54 +0400

    add list organism

---
+++ src/atomic/organism/icon-title-text-list.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import IconTitleTextElem, { IconTitleText } from "../molecule/icon-title-text-elem"
+
+// type IconTitleTextListProps = {
+// 	iconList: Array<string>
+// 	titleList: Array<string>
+// 	subtitleList: Array<string>
+// }
+
+interface IIconTitleTextListProps {
+	items: Array<IconTitleText>
+}
+
+const IconTitleTextList = ({
+	items,
+}: IIconTitleTextListProps): JSX.Element => {
+	return (
+		<ul className="list-with-icons">
+			{items.map((item, index) => (
+				<IconTitleTextElem
+					key={index}
+					{...item}
+				/>
+			))}
+		</ul>
+	)
+}
+
+export default IconTitleTextList
