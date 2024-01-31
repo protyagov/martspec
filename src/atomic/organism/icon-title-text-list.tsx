751980279d852c49aa15d9a6e35cfc7fe5290edf
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-01-31 18:14:24 +0400

    fix formatting

--- src/atomic/organism/icon-title-text-list.tsx
+++ src/atomic/organism/icon-title-text-list.tsx
@@ -1,29 +1,23 @@
 import React from "react"
 import IconTitleTextElem, {
-	IconTitleText,
+    IconTitleText,
 } from "../molecule/icon-title-text-elem"
 
-// type IconTitleTextListProps = {
-// 	iconList: Array<string>
-// 	titleList: Array<string>
-// 	subtitleList: Array<string>
-// }
-
 interface IIconTitleTextListProps {
-	items: Array<IconTitleText>
+    items: Array<IconTitleText>
 }
 
 const IconTitleTextList = ({ items }: IIconTitleTextListProps): JSX.Element => {
-	return (
-		<ul className="list-with-icons">
-			{items.map((item, index) => (
-				<IconTitleTextElem
-					key={index}
-					{...item}
-				/>
-			))}
-		</ul>
-	)
+    return (
+        <ul className="list-with-icons">
+            {items.map((item, index) => (
+                <IconTitleTextElem
+                    key={index}
+                    {...item}
+                />
+            ))}
+        </ul>
+    )
 }
 
 export default IconTitleTextList
--- src/atomic/page/vitamin.tsx
+++ src/atomic/page/vitamin.tsx
@@ -17,58 +17,57 @@ const vitamins = {
 
 export default function Vitamin() {
     // const bgColor={background: "#1FF"}
-    // <--
     const iconTitleTextListItems: Array<IconTitleText> = [
-			{
-				icon: {
-					src: "/img/atom/icons/icon-defense.svg",
-					alt: _("VITAMIN.ALT5_1"),
-				},
-				title: _("VITAMIN.LIST4.LI1_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI1_TEXT"),
-			},
-			{
-				icon: {
-					src: "/img/atom/icons/icon-sync.svg",
-					alt: _("VITAMIN.ALT5_2"),
-				},
-				title: _("VITAMIN.LIST4.LI2_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI2_TEXT"),
-			},
-			{
-				icon: {
-					src: "/img/atom/icons/icon-no-auth.svg",
-					alt: _("VITAMIN.ALT5_3"),
-				},
-				title: _("VITAMIN.LIST4.LI3_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI3_TEXT"),
-			},
-			{
-				icon: {
-					src: "/img/atom/icons/icon-no-ads.svg",
-					alt: _("VITAMIN.ALT5_4"),
-				},
-				title: _("VITAMIN.LIST4.LI4_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI4_TEXT"),
-			},
-			{
-				icon: {
-					src: "/img/atom/icons/icon-bell.svg",
-					alt: _("VITAMIN.ALT5_5"),
-				},
-				title: _("VITAMIN.LIST4.LI5_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI5_TEXT"),
-			},
-			{
-				icon: {
-					src: "/img/atom/icons/icon-apple-health.svg",
-					alt: _("VITAMIN.ALT5_5"),
-				},
-				title: _("VITAMIN.LIST4.LI6_HEAD"),
-				subtitle: _("VITAMIN.LIST4.LI6_TEXT"),
-			},
-		]
-    // -->
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-defense.svg",
+                    alt: _("VITAMIN.ALT5_1"),
+                },
+                title: _("VITAMIN.LIST4.LI1_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI1_TEXT"),
+            },
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-sync.svg",
+                    alt: _("VITAMIN.ALT5_2"),
+                },
+                title: _("VITAMIN.LIST4.LI2_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI2_TEXT"),
+            },
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-no-auth.svg",
+                    alt: _("VITAMIN.ALT5_3"),
+                },
+                title: _("VITAMIN.LIST4.LI3_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI3_TEXT"),
+            },
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-no-ads.svg",
+                    alt: _("VITAMIN.ALT5_4"),
+                },
+                title: _("VITAMIN.LIST4.LI4_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI4_TEXT"),
+            },
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-bell.svg",
+                    alt: _("VITAMIN.ALT5_5"),
+                },
+                title: _("VITAMIN.LIST4.LI5_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI5_TEXT"),
+            },
+            {
+                icon: {
+                    src: "/img/atom/icons/icon-apple-health.svg",
+                    alt: _("VITAMIN.ALT5_5"),
+                },
+                title: _("VITAMIN.LIST4.LI6_HEAD"),
+                subtitle: _("VITAMIN.LIST4.LI6_TEXT"),
+            },
+        ]
+    
     return <>
         <NavigationBar />
 
