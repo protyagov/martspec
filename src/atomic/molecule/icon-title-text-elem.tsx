ce9c96adc7c3780f79e3cec7254963d49dac7ce7
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-01-31 18:54:45 +0400

    refactor

--- src/atomic/molecule/icon-title-text-elem.tsx
+++ src/atomic/molecule/icon-title-text-elem.tsx
@@ -1,41 +1,44 @@
 import React from "react"
 
 interface Icon {
-    src: string
-    alt: string
+	src: string
+	alt: string
 }
 
 export interface IconTitleText {
-    icon: Icon
-    title: string
-    subtitle: string
+	icon: Icon
+	title: string
+	subtitle: string
 }
 
 interface IconTitleTextElemProps extends IconTitleText {}
 
 const IconTitleTextElem = ({
-    icon,
-    title,
-    subtitle,
+	icon,
+	title,
+	subtitle,
 }: IconTitleTextElemProps): JSX.Element => {
-    const appleIconName = "icon-apple-health.svg"
-    const appleIconClass = "no-filter"
+	const appleIconName = "icon-apple-health.svg"
+	const appleIconClass = "no-filter"
 
-    return (
-        <li>
-            <div
-                className={`icon${
-                    icon.src.includes(appleIconName) ? ` ${appleIconClass}` : ""
-                }`}
-            >
-                <img {...icon} />
-            </div>
-            <div>
-                <h3>{title}</h3>
-                <p>{subtitle}</p>
-            </div>
-        </li>
-    )
+	return (
+		<li>
+			<div
+				className={`icon${
+					icon.src.includes(appleIconName) ? ` ${appleIconClass}` : ""
+				}`}
+			>
+				<img
+					src={icon.src}
+					alt={icon.alt}
+				/>
+			</div>
+			<div>
+				<h3>{title}</h3>
+				<p>{subtitle}</p>
+			</div>
+		</li>
+	)
 }
 
 export default IconTitleTextElem
