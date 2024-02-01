a8a19d97f49e4c51eed16812acd471aff1d9e5fc
protyagov <protyagov@gmail.com>
2024-01-31 23:12:59 -0600

    Updates

--- atomic/atom/icon-with-load.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-export default function IconWithLoad(props) {
-    if (props.isLoading)
-        return React.createElement("i", { className: "fo icon-spin6 animate-spin" });
-    return React.createElement("i", { className: props.isActive ? props.classActive : props.classPassive });
-}
-//# sourceMappingURL=icon-with-load.js.map
--- atomic/atom/img-i18n.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Locale } from "src/i18n/locale";
-export default function ImageI18N(props) {
-    const loImg = props.src.replace("-en.", `-${Locale.language}.`);
-    return React.createElement("img", { src: loImg, width: props.w, height: props.h, className: props.cls, alt: props.alt, onError: e => e.currentTarget.src = props.src });
-}
-//# sourceMappingURL=img-i18n.js.map
