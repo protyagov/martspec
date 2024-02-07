7d11c7c7f7478dd25beab414587f0fdb13266ae6
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-07 20:10:21 +0400

    add TitleTextCard molecule

---
+++ src/atomic/molecule/title-text-card.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+interface TitleTextCardProps {
+	title: string
+	text: string
+	bgColor?: string
+	className?: string
+}
+
+const TitleTextCard = ({
+	title,
+	text,
+	bgColor = "#FFFFFF",
+	className = "col-lg-4",
+}: TitleTextCardProps): JSX.Element => {
+	return (
+		<div className={className}>
+			<div
+				className="block"
+				style={{ backgroundColor: bgColor }}
+			>
+				<h3>{title}</h3>
+				<p>{text}</p>
+			</div>
+		</div>
+	)
+}
+
+export default TitleTextCard
