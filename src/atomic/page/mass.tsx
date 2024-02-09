784c4acfde162e2e2fe100386482ab78d4942ea4
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-09 21:23:37 +0400

    redesign of bodymass page (without images)

--- src/atomic/page/mass.tsx
+++ src/atomic/page/mass.tsx
@@ -1,64 +1,137 @@
-import * as React from "react";
-import _, { Locale } from "src/i18n/locale";
-import { Footer } from "src/atomic/organism/footer";
-import NavigationBar from "src/atomic/organism/navbar";
-import PageHeader from "../molecule/page-header";
+import * as React from "react"
+import _, { Locale } from "src/i18n/locale"
+import { Footer } from "src/atomic/organism/footer"
+import NavigationBar from "src/atomic/organism/navbar"
+import PageHeader from "../molecule/page-header"
+import TitleTextCard from "../molecule/title-text-card"
+import ImageI18N from "../atom/img-i18n"
+import IconTitleTextList from "../organism/icon-title-text-list"
+import { IconTitleText } from "../molecule/icon-title-text-elem"
+import CallToAction from "../organism/call-to-action"
 
 export default function Bodymass() {
-    return <>
-        <NavigationBar />
+	const iconTitleTextListItems: Array<IconTitleText> = [
+		{
+			icon: {
+				src: "/img/atom/icons/icon-defense.svg",
+				alt: _("MASS.ALT5_1"),
+			},
+			title: _("MASS.LIST3.LI1_HEAD"),
+			subtitle: _("MASS.LIST3.LI1_TEXT"),
+		},
+		{
+			icon: {
+				src: "/img/atom/icons/icon-sync.svg",
+				alt: _("MASS.ALT5_2"),
+			},
+			title: _("MASS.LIST3.LI2_HEAD"),
+			subtitle: _("MASS.LIST3.LI2_TEXT"),
+		},
+		{
+			icon: {
+				src: "/img/atom/icons/icon-no-auth.svg",
+				alt: _("MASS.ALT5_3"),
+			},
+			title: _("MASS.LIST3.LI3_HEAD"),
+			subtitle: _("MASS.LIST3.LI3_TEXT"),
+		},
+		{
+			icon: {
+				src: "/img/atom/icons/icon-no-ads.svg",
+				alt: _("MASS.ALT5_4"),
+			},
+			title: _("MASS.LIST3.LI4_HEAD"),
+			subtitle: _("MASS.LIST3.LI4_TEXT"),
+		},
+		{
+			icon: {
+				src: "/img/atom/icons/icon-apple-health.svg",
+				alt: _("MASS.ALT5_5"),
+			},
+			title: _("MASS.LIST3.LI5_HEAD"),
+			subtitle: _("MASS.LIST3.LI5_TEXT"),
+		},
+	]
+	return (
+		<>
+			<NavigationBar />
 
-        <div className="ms-base-page pb-5">
+			<div className="ms-base-page ms-base-new bodymass">
+				<PageHeader
+					title="MASS.HEAD"
+					subtitleLevel1="MASS.ABOUT_1"
+					subtitleLevel2="MASS.ABOUT_2"
+					appId={1507588203}
+					appDownloadTitle="MASS.DWN"
+					imgSrc="/img/page/body-mass/mass.svg"
+					imgAlt="MASS.IMG"
+					imgH={406}
+					imgW={512}
+				/>
 
-            <PageHeader
-                title="MASS.HEAD"
-                subtitleLevel2="MASS.ABOUT"
-                appId={1507588203}
-                appDownloadTitle="MASS.DWN"
-                imgSrc="/img/page/body-mass/mass.svg"
-                imgAlt="MASS.IMG"
-                imgH={406}
-                imgW={512}
-            />
+				<section>
+					<div className="row g-4">
+						<TitleTextCard
+							title={_("MASS.LIST1.LI1_HEAD")}
+							text={_("MASS.LIST1.LI1_TEXT")}
+							bgColor="#E8FAE0"
+						/>
+						<TitleTextCard
+							title={_("MASS.LIST1.LI2_HEAD")}
+							text={_("MASS.LIST1.LI2_TEXT")}
+							bgColor="#D6E3FC"
+						/>
+						<TitleTextCard
+							title={_("MASS.LIST1.LI3_HEAD")}
+							text={_("MASS.LIST1.LI3_TEXT")}
+							bgColor="#E8FAE0"
+						/>
+					</div>
+				</section>
 
-            <section>
-                <h4>{_("MASS.HEAD1")}</h4>
-                <p className="mt-3">{_("MASS.DESC1")}</p>
-            </section>
+				<section>
+					<div className="row">
+						<div className="col-lg-6">
+							<h2>{_("MASS.HEAD2")}</h2>
+							<p className="mb-4">{_("MASS.DESC2")}</p>
+						</div>
+						<div className="col-lg-6">
+							<ImageI18N
+								src="/img/page/vitamin/vitamin-screen-desc-en.webp"
+								w={390}
+								h={515}
+								cls="ms-base-image mt-mob-xs"
+								alt={_("MASS.ALT1")}
+							/>
+						</div>
+					</div>
+				</section>
 
-            <section>
-                <h4>{_("MASS.HEAD2")}</h4>
-                <p className="mt-3">{_("MASS.DESC2")}</p>
-            </section>
+				<section>
+					<div className="row">
+						<div className="col-12 mb-2">
+							<h2>{_("VITAMIN.HEAD4")}</h2>
+						</div>
+						<div className="col-12">
+							<IconTitleTextList items={iconTitleTextListItems} />
+						</div>
+					</div>
+				</section>
 
-            <section>
-                <h4>{_("MASS.HEAD3")}</h4>
-                <p className="mt-3">{_("MASS.DESC3")}</p>
-            </section>
+				<section>
+					<CallToAction
+						bgColor={{backgroundColor: "#E8FAE0"}}
+						title={_("MASS.HEAD4")}
+						subtitle={_("MASS.DESC4")}
+						appId={1507588203}
+						appDownloadTitle={_("MASS.DWN")}
+						imgSrc="/img/org/call-to-action/vitamin/vitamin-screen-app-en.webp"
+						imgAlt={_("VITAMIN.ALT4")}
+					/>
+				</section>
+			</div>
 
-            <section>
-                <h4>{_("MASS.HEAD4")}</h4>
-                <p className="mt-3">
-                    <ul>
-                        <li>{_("MASS.DESC4_1")}</li>
-                        <li>{_("MASS.DESC4_2")}</li>
-                        <li>{_("MASS.DESC4_3")}</li>
-                        <li>{_("MASS.DESC4_4")}</li>
-                        <li>{_("MASS.DESC4_5")}</li>
-                        <li>{_("MASS.DESC4_6")}</li>
-                        <li>{_("MASS.DESC4_7")}</li>
-                    </ul>
-                </p>
-            </section>
-
-
-        </div>
-
-
-        <div className="container mb-5 pb-5 text-center">
-            <img src="/img/page/body-mass/mass-app.webp" alt={_("MAIN.P2.IMG")} className="ms-base-image" width="512px" height="512px" />
-        </div>
-
-        <Footer />
-    </>
+			<Footer />
+		</>
+	)
 }
