a21ebe06df0a71aaddde3676444b8e29ca2fa491
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-02-23 18:56:03 +0400

    add page content

--- src/atomic/page/careers.tsx
+++ src/atomic/page/careers.tsx
@@ -1,7 +1,119 @@
 import React from "react"
+import _, { Locale } from "src/i18n/locale"
+import NavigationBar from "../organism/navbar"
+import { Footer } from "../organism/footer"
+import TitleTextCard from "../molecule/title-text-card"
+import TitleTextLinkCard from "../molecule/title-text-link-card"
 
 const Careers = () => {
-	return <div>careers</div>
+	return (
+		<>
+			<NavigationBar />
+			<div className="ms-base-page ms-base-new">
+				<section className="text-center">
+					<div className="row">
+						<div className="col">
+							<div className="headings">
+								<h1>{_("CAREERS.HEAD")}</h1>
+								<h2>{_("CAREERS.ABOUT")}</h2>
+								<img
+									src={"/img/page/careers/main-pic-top.webp"}
+									height={476}
+									width={667}
+									className="ms-base-image"
+									alt={_("CAREERS.IMG")}
+								/>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section>
+					<div className="row">
+						<div className="col-lg-6">
+							<h2>{_("CAREERS.HEAD1")}</h2>
+							<p className="mb-4">{_("CAREERS.TEXT1_1")}</p>
+							<p className="mb-4">{_("CAREERS.TEXT1_2")}</p>
+							<p className="mb-4">{_("CAREERS.TEXT1_3")}</p>
+						</div>
+						<div className="col-lg-6">
+							<img
+								src="/img/page/careers/main-pic-middle.webp"
+								width={600}
+								height={429}
+								className="ms-base-image mt-mob-xs"
+								alt={_("CAREERS.ALT1")}
+							/>
+						</div>
+					</div>
+				</section>
+
+				<section>
+					<div className="row g-4">
+						<h2>{_("CAREERS.HEAD2")}</h2>
+						<TitleTextCard
+							title={_("CAREERS.LIST2.LI1_HEAD")}
+							text={_("CAREERS.LIST2.LI1_TEXT")}
+							bgColor="#FFB34066"
+						/>
+						<TitleTextCard
+							title={_("CAREERS.LIST2.LI2_HEAD")}
+							text={_("CAREERS.LIST2.LI2_TEXT")}
+							bgColor="#3D9AD166"
+						/>
+						<TitleTextCard
+							title={_("CAREERS.LIST2.LI3_HEAD")}
+							text={_("CAREERS.LIST2.LI3_TEXT")}
+							bgColor="#FFB34066"
+						/>
+					</div>
+				</section>
+
+				<section>
+					<div className="row g-4">
+						<h2>{_("CAREERS.HEAD3")}</h2>
+						<TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI1_HEAD")}
+							text={_("CAREERS.LIST3.LI1_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-webdesigner.webp"
+						/>
+                        <TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI2_HEAD")}
+							text={_("CAREERS.LIST3.LI2_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-mobiledesigner.webp"
+						/>
+                        <TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI3_HEAD")}
+							text={_("CAREERS.LIST3.LI3_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-frontend.webp"
+						/>
+                        <TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI4_HEAD")}
+							text={_("CAREERS.LIST3.LI4_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-qa.webp"
+						/>
+                        <TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI5_HEAD")}
+							text={_("CAREERS.LIST3.LI5_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-ba.webp"
+						/>
+                        <TitleTextLinkCard
+							title={_("CAREERS.LIST3.LI6_HEAD")}
+							text={_("CAREERS.LIST3.LI6_TEXT")}
+							link={{ href: "/coming-soon", text: _("CAREERS.BUTTON3") }}
+                            image="/img/page/careers/vacancy-pic-copywriter.webp"
+						/>
+					</div>
+				</section>
+			</div>
+			<Footer />
+		</>
+	)
 }
 
 export default Careers
