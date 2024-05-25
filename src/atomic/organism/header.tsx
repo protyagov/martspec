2ca4a6b3fe254308239d49d7a5de96e41bfc54b5
Ljoskold <128295295+Ljoskold@users.noreply.github.com>
2024-05-25 17:03:58 +0700

    add alias import, delete ms-base, renamed section

--- src/atomic/organism/header.tsx
+++ src/atomic/organism/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import _, { Locale } from "@/i18n/locale";
 import ButtonApple from "@/atomic/atom/button-apple";
-import ImageI18N from "../atom/img-i18n";
+import ImageI18N from "@/atomic/atom/img-i18n";
 
 type HeaderProps = {
     title: string;
@@ -28,21 +28,17 @@ export default function Header(props: WithImage<HeaderProps> | WithoutImage<Head
     const { title, imgSrc, imgH, imgW, imgAlt, appId, appDownloadTitle, content } = props;
 
     return (
-        <div className="ms-base-new">
-            <section className="page-header text-center">
-                <div className="row">
-                    <div className="col">
-                        <div className="headings">
-                            <h1>{title}</h1>
-                            {content}
-                            {appId && <ButtonApple appId={appId} appDownloadTitle={appDownloadTitle} />}
-                        </div>
-                        {imgSrc && (
-                            <ImageI18N src={imgSrc} h={imgH} w={imgW} cls="ms-base-image header-main" alt={imgAlt} />
-                        )}
+        <section className="new-page-header">
+            <div className="row">
+                <div className="col">
+                    <div className="headings">
+                        <h1>{title}</h1>
+                        {content}
+                        {appId && <ButtonApple appId={appId} appDownloadTitle={appDownloadTitle} />}
                     </div>
+                    {imgSrc && <ImageI18N src={imgSrc} h={imgH} w={imgW} cls="header-main-image" alt={imgAlt} />}
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 }
