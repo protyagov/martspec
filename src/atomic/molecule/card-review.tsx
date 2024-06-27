061e2e6eb2e80711bdb1ca1874ce62c45126a8ac
EGRrqq <egrrqqdev@gmail.com>
2024-06-27 04:58:54 -0400

    [MS-1229] add rating with star icon

--- src/atomic/molecule/card-review.tsx
+++ src/atomic/molecule/card-review.tsx
@@ -1,18 +1,28 @@
 import React from "react";
+import StarIcon from "@/atomic/atom/star-icon";
 
 interface ICardReviewProps {
     reviewerNickname: string;
     createdDate: string;
     reviewText: string;
+    rating: number;
 }
 
 const formatDate = (date: string) => new Date(date).toLocaleDateString("ru-RU");
 
-export default function CardReview({ reviewerNickname, createdDate, reviewText }: ICardReviewProps) {
+export default function CardReview({ reviewerNickname, createdDate, reviewText, rating }: ICardReviewProps) {
     return (
         <section>
             <header>
-                <h3>{reviewerNickname}</h3>
+                <div>
+                    <h3>{reviewerNickname}</h3>
+
+                    <div>
+                        <StarIcon />
+
+                        <span>{rating},0</span>
+                    </div>
+                </div>
                 <time dateTime={createdDate}>{formatDate(createdDate)}</time>
             </header>
 
