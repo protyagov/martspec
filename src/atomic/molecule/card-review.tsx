660f486d221ef8c0fa306fa4154af42b16aa937d
EGRrqq <egrrqqdev@gmail.com>
2024-06-27 04:16:33 -0400

    [MS-1229] pass review data into card component

--- src/atomic/molecule/card-review.tsx
+++ src/atomic/molecule/card-review.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function CardReview() {
+interface ICardReviewProps {
+    reviewerNickname: string;
+    createdDate: string;
+    reviewText: string;
+}
+
+const formatDate = (date: string) => new Date(date).toLocaleDateString("ru-RU");
+
+export default function CardReview({ reviewerNickname, createdDate, reviewText }: ICardReviewProps) {
     return (
         <section>
             <header>
-                <h3>nickname</h3>
-                <time dateTime="">04.11.2024</time>
+                <h3>{reviewerNickname}</h3>
+                <time dateTime={createdDate}>{formatDate(createdDate)}</time>
             </header>
 
-            <p>review text</p>
+            <p>{reviewText}</p>
         </section>
     );
 }
