b8c32ce4a2e9209c4e5918e59859eeb66cf7e227
Alexandra Paramonova <alpar.wb@gmail.com>
2024-06-28 09:15:06 +0500

    [MS-1225] add CardImage molecule

---
+++ src/atomic/molecule/card-image.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ImageI18N from "@/atomic/atom/img-i18n";
+
+interface CardImageProps {
+    imgSrc: string;
+    imgH: number;
+    imgW: number;
+    imgAlt?: string;
+    bgColor?: string;
+    className?: string;
+    alignItems?: "start" | "center" | "end";
+    justifyContent?: "start" | "center" | "end";
+}
+
+export default function CardImage({
+    imgSrc,
+    imgH,
+    imgW,
+    imgAlt,
+    bgColor,
+    className = "col-lg-4",
+    alignItems = "center",
+    justifyContent = "center",
+}: CardImageProps) {
+    return (
+        <div className={className}>
+            <div
+                className={`d-flex h-100 rounded-5 align-items-${alignItems} justify-content-${justifyContent}`}
+                {...(bgColor && { style: { backgroundColor: bgColor } })}
+            >
+                <ImageI18N src={imgSrc} w={imgW} h={imgH} alt={imgAlt} cls="img-fluid m-0" />
+            </div>
+        </div>
+    );
+}
