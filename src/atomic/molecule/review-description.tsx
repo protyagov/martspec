06563d90798666f0d3f7c36324064715299b5d3b
EGRrqq <egrrqqdev@gmail.com>
2024-07-04 04:51:37 -0400

    [MS-1229] move review content into separate components
    for ease of use with useMedia query hook
    
    also a good apportunity make compound review component

---
+++ src/atomic/molecule/review-description.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import _ from "@/i18n/locale";
+
+export default function ReviewDescription() {
+    return <p className="review__description">{_("VITAMIN.REVIEW.DESCRIPTION")}</p>;
+}
