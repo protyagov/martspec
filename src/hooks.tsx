0a1a9c7d81dc1e9312a81412812fba149feb320d
EGRrqq <egrrqqdev@gmail.com>
2024-07-04 04:08:08 -0400

    [MS-1229] add useMediaQuery hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,5 +1,5 @@
 import { ScrollSpy } from "bootstrap";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const useScrollSpy = (scrollTargetId: string, scrollContainerId: string = "root") => {
     const scrollContainer = document.getElementById(scrollContainerId);
@@ -9,4 +9,27 @@ export const useScrollSpy = (scrollTargetId: string, scrollContainerId: string =
         const target = scrollTargetId.replace(/^[^#]+/, "#$&");
         const scrollSpy = new ScrollSpy(scrollContainer, { target });
     }, []);
+};
+
+//-------------------------------------------------------------------------------
+
+export const useMediaQuery = (query: string): boolean => {
+    const [match, setMatches] = useState(false);
+
+    // set match on mount
+    useEffect(() => {
+        setMatches(window.matchMedia(query).matches);
+    }, []);
+
+    // handle match event
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(query);
+        const matchHandler = (e: MediaQueryListEvent) => setMatches(e.matches);
+
+        mediaQuery.addEventListener("change", matchHandler);
+
+        return () => mediaQuery.removeEventListener("change", matchHandler);
+    }, []);
+
+    return match;
 };
