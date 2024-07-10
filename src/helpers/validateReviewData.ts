82770b1ac164c342aabf14cbbc313d04153928b9
EGRrqq <egrrqqdev@gmail.com>
2024-07-10 08:38:32 -0400

    [MS-1229] add documentation for review validation

--- src/helpers/validateReviewData.ts
+++ src/helpers/validateReviewData.ts
@@ -9,18 +9,23 @@ interface IValidateReviewData {
 
 type TValidateReviewData = (props: IValidateReviewData) => Promise<IReviewWithFiller>;
 
+// with validation it always returns an array with review or filler data
 export const validateReviewData: TValidateReviewData = async ({ reviewData, arrLength }) => {
+    // if no data return fillers
     if (!reviewData) {
         return fillerArray(arrLength);
     }
 
+    // if review is a single object return data + fillers
     if (!Array.isArray(reviewData)) {
         return [reviewData, ...fillerArray(arrLength - 1)];
     }
 
+    // if review is array but length less than needed return data + fillers
     if (reviewData.length < arrLength) {
         return [...reviewData, ...fillerArray(arrLength - reviewData.length)];
     }
 
+    // by default, return a certain number of reviews
     return reviewData.slice(0, arrLength);
 };
