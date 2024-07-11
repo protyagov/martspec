17b53cc3c6e23a17b812547bb765535324764a02
EGRrqq <egrrqqdev@gmail.com>
2024-07-11 11:45:24 -0400

    [MS-1229] review component determines app id by url

--- src/hooks/useReviewData.ts
+++ src/hooks/useReviewData.ts
@@ -1,20 +1,17 @@
 import { useEffect, useState } from "react";
-import { Locale } from "@/i18n/locale";
 
 import { IReviewWithFiller } from "@/data/IReviewWithFiller";
-import { fillerArray } from "@/helpers/fillerArray";
 import { getReviewData } from "@/helpers/getReviewData";
 import { validateReviewData } from "@/helpers/validateReviewData";
 
-const ID_VITAMIN = "1519596234";
 const ARR_LENGTH = 3;
 
 export const useReviewData = () => {
-    const [reviews, setReviews] = useState<IReviewWithFiller>(fillerArray(ARR_LENGTH));
+    const [reviews, setReviews] = useState<IReviewWithFiller | null>(null);
 
     useEffect(() => {
-        getReviewData({ id: ID_VITAMIN, country_code: Locale.countryCode })
-            .then((r) => validateReviewData({ reviewData: r.feed.entry, arrLength: ARR_LENGTH }))
+        getReviewData()
+            .then((r) => (r.data ? validateReviewData({ reviewData: r.data.feed.entry, arrLength: ARR_LENGTH }) : null))
             .then((r) => setReviews(r));
     }, []);
 
