a32306bf771ac2feacb3e2a7df37bf7f40d55241
renata tsedenova <recedenova@gmail.com>
2024-07-12 09:46:30 +0300

    Temporary commit for img.sh before pulling changes

--- src/img/img.sh
+++ src/img/img.sh
@@ -1,73 +1,30 @@
 #!/bin/bash
 
-FILES=*.png
-for f in org/$FILES
-do
-# 	sips -Z 64  $f --out $f.size64
-# 	pngcrush -brute $f.size64 $f.size64.new		
-#  	cwebp -lossless -z 9 -m 6 -mt -hint picture -metadata none $f.size64.new -o "${f:4:${#f}-8}".64.webp 	
-  	
+# source directory with the original image files, relative to the script folder
+DIR_INPUT=new
+# output directory relative to the script folder
+DIR_OUT=
 
-	sips -Z 512  $f --out $f.size512
-	pngcrush -brute $f.size512 $f.size512.new		
-  	cwebp -lossless -z 9 -m 6 -mt -hint picture -metadata none $f.size512.new -o "${f:4:${#f}-8}".512.webp 	
+# maximum size in px for the image height and width
+MAX_SIZE=512
+FILES=*.png
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+for f in $SCRIPT_DIR/$DIR_INPUT/$FILES
+do
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		sips -Z "${MAX_SIZE}"  $f --out $f.size"${MAX_SIZE}"
+	else
+	  size="${MAX_SIZE}x${MAX_SIZE}"
+		magick $f -resize "${size}"\> $f.size"${MAX_SIZE}"
+	fi
+
+	pngcrush -brute $f.size"${MAX_SIZE}" $f.size"${MAX_SIZE}".new
+
+	filename_with_ext="${f##*/}"
+	filename="${filename_with_ext%.*}"
+	cwebp -lossless -z 9 -m 6 -mt -hint picture -metadata none $f.size"${MAX_SIZE}".new -o $SCRIPT_DIR/$DIR_OUT/"${filename}.${MAX_SIZE}".webp
 done
 
-rm org/*size512
-rm org/*.new
-
-cp org/header_watches.webp .
-
-cp vitamin.en.512.webp vitamin.ar.512.webp
-cp vitamin.en.512.webp vitamin.de.512.webp
-cp vitamin.en.512.webp vitamin.es.512.webp
-cp vitamin.en.512.webp vitamin.fr.512.webp
-cp vitamin.en.512.webp vitamin.hi.512.webp
-cp vitamin.en.512.webp vitamin.ja.512.webp
-cp vitamin.en.512.webp vitamin.pt.512.webp
-cp vitamin.en.512.webp vitamin.uk.512.webp
-
-cp size.en.512.webp size.ar.512.webp
-cp size.en.512.webp size.de.512.webp
-cp size.en.512.webp size.es.512.webp
-cp size.en.512.webp size.fr.512.webp
-cp size.en.512.webp size.hi.512.webp
-cp size.en.512.webp size.ja.512.webp
-cp size.en.512.webp size.pt.512.webp
-cp size.en.512.webp size.uk.512.webp
-
-cp header_screens.en.512.webp header_screens.ar.512.webp
-cp header_screens.en.512.webp header_screens.de.512.webp
-cp header_screens.en.512.webp header_screens.es.512.webp
-cp header_screens.en.512.webp header_screens.fr.512.webp
-cp header_screens.en.512.webp header_screens.hi.512.webp
-cp header_screens.en.512.webp header_screens.ja.512.webp
-cp header_screens.en.512.webp header_screens.pt.512.webp
-cp header_screens.en.512.webp header_screens.uk.512.webp
-
-cp mass.en.512.webp mass.ar.512.webp
-cp mass.en.512.webp mass.de.512.webp
-cp mass.en.512.webp mass.es.512.webp
-cp mass.en.512.webp mass.fr.512.webp
-cp mass.en.512.webp mass.hi.512.webp
-cp mass.en.512.webp mass.ja.512.webp
-cp mass.en.512.webp mass.pt.512.webp
-cp mass.en.512.webp mass.uk.512.webp
-
-cp emotion.en.512.webp emotion.ar.512.webp
-cp emotion.en.512.webp emotion.de.512.webp
-cp emotion.en.512.webp emotion.es.512.webp
-cp emotion.en.512.webp emotion.fr.512.webp
-cp emotion.en.512.webp emotion.hi.512.webp
-cp emotion.en.512.webp emotion.ja.512.webp
-cp emotion.en.512.webp emotion.pt.512.webp
-cp emotion.en.512.webp emotion.uk.512.webp
-
-cp waist.en.512.webp waist.ar.512.webp
-cp waist.en.512.webp waist.de.512.webp
-cp waist.en.512.webp waist.es.512.webp
-cp waist.en.512.webp waist.fr.512.webp
-cp waist.en.512.webp waist.hi.512.webp
-cp waist.en.512.webp waist.ja.512.webp
-cp waist.en.512.webp waist.pt.512.webp
-cp waist.en.512.webp waist.uk.512.webp
+rm $SCRIPT_DIR/$DIR_INPUT/*size"${MAX_SIZE}"
+rm $SCRIPT_DIR/$DIR_INPUT/*.new
