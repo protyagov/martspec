de39259dfb84dab6cc609975e753c07ad139ee38
EGRrqq <egrrqqdev@gmail.com>
2024-07-28 02:57:19 -0400

    make story step

---
+++ src/stories/CardTitleText.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { Story } from "@ladle/react";
+import CardTitleText from "@/atomic/molecule/card-title-text";
+
+export const CardTitleText_Story: Story = () => (
+    <CardTitleText title="STORY PREVIEW" text="TEXT" bgColor="#E5F4D9" textColor="#151515" />
+);
