54668e585f3649b47e6d2bbcacbe24c9d5964001
EGRrqq <egrrqqdev@gmail.com>
2024-07-28 11:42:17 -0400

    react cosmos cfg, failure
    ERROR in Conflict: Multiple assets emit different content to the same filename renderer.html

---
+++ src/stories/CardTitleText.fixture.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+import CardTitleText from "../atomic/molecule/card-title-text";
+
+export default <CardTitleText title="STORY PREVIEW" text="TEXT" bgColor="#E5F4D9" textColor="#151515" />;
