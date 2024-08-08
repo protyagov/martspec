af83fcaaec14d133bb96e46172ba273fc87d924d
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-08 21:03:58 +0500

    [MS-1224] correct text width with padding in CardTitleTextImage molecule

--- src/atomic/molecule/card-title-text-image.tsx
+++ src/atomic/molecule/card-title-text-image.tsx
@@ -26,7 +26,7 @@ export default function CardTitleTextImage({
                 <h3 className="mb-3 pe-5 fs-4 lh-sm" style={{ color: textColor }}>
                     {title}
                 </h3>
-                <p className="mb-0 pe-4 fs-5 lh-sm" style={{ color: textColor }}>
+                <p className="mb-0 pe-5 fs-5 lh-sm" style={{ color: textColor }}>
                     {text}
                 </p>
             </div>
