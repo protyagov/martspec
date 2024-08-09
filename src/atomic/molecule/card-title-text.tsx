c30129c8e99648aaa464655638c11c75c1af22ee
Alexandra Paramonova <138000021+learnwbdev@users.noreply.github.com>
2024-08-09 21:05:48 +0500

    [MS-1224] fix font-weight and default text color in CardTitleTextImage and CardTitleText molecules (#500)
    * [MS-1224] correct text color and font weight in CardTitleTextImage molecule
    
    * [MS-1224] correct text color and font weight in CardTitleText molecule
    
    * [MS-1224] remove text color setting in vitamin page for CardTitleTextImage and CardTitleText

--- src/atomic/molecule/card-title-text.tsx
+++ src/atomic/molecule/card-title-text.tsx
@@ -1,19 +1,27 @@
 import React, { CSSProperties } from "react";
+import "@/sass/molecule/card-title-text.scss";
 
 interface CardTitleTextProps {
     title: string;
     text: string;
     bgColor?: CSSProperties["color"];
+    titleColor?: CSSProperties["color"];
     textColor?: CSSProperties["color"];
 }
 
-export default function CardTitleText({ title, text, bgColor, textColor }: CardTitleTextProps): React.ReactNode {
+export default function CardTitleText({
+    title,
+    text,
+    bgColor,
+    titleColor = "#212529",
+    textColor = "#595959",
+}: CardTitleTextProps): React.ReactNode {
     return (
-        <div className="h-100 rounded-5 p-lg-5 p-4 pb-5" style={{ backgroundColor: bgColor }}>
-            <h3 className="mb-3 pe-5 fs-4 lh-sm" style={{ color: textColor }}>
+        <div className="card-title-text h-100 rounded-5 p-lg-5 p-4 pb-5" style={{ backgroundColor: bgColor }}>
+            <h3 className="mb-3 pe-5 fs-4 lh-sm" style={{ color: titleColor }}>
                 {title}
             </h3>
-            <p className="mb-0 pe-4 fs-5 lh-sm" style={{ color: textColor }}>
+            <p className="text mb-0 pe-4 fs-5 lh-sm" style={{ color: textColor }}>
                 {text}
             </p>
         </div>
