ecb16a41a03827966a5c859e8cc72b4277a2c3df
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-25 12:25:28 +0500

    add color and hoverColor props to text-link-arrow

--- src/atomic/molecule/text-link-arrow.tsx
+++ src/atomic/molecule/text-link-arrow.tsx
@@ -1,13 +1,27 @@
-import React, { ReactNode } from "react";
+import React, { CSSProperties, ReactNode } from "react";
+import "@/sass/molecule/text-link-arrow.scss";
+
+interface CSSPropertiesWithVars extends CSSProperties {
+    "--hover-color"?: CSSProperties["color"];
+}
 
 interface TextLinkArrowProps {
     href: string;
     rightIcon: ReactNode;
     text: string | ReactNode;
     isNewTab?: boolean;
+    color?: CSSProperties["color"];
+    hoverColor?: CSSProperties["color"];
 }
 
-export default function TextLinkArrow({ rightIcon, text, href, isNewTab }: TextLinkArrowProps) {
+export default function TextLinkArrow({
+    rightIcon,
+    text,
+    href,
+    isNewTab,
+    color = "#0d6efd",
+    hoverColor = "#0a58ca",
+}: TextLinkArrowProps) {
     const newTabProps = isNewTab && { target: "_blank", rel: "noreferrer noopener" };
 
     return (
@@ -15,6 +29,7 @@ export default function TextLinkArrow({ rightIcon, text, href, isNewTab }: TextL
             href={href}
             {...newTabProps}
             className="text-link-arrow d-flex align-items-center gap-2 text-decoration-none lh-sm"
+            style={{ color, "--hover-color": hoverColor } as CSSPropertiesWithVars}
         >
             {text}
             {rightIcon}
