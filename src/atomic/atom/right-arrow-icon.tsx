2933bc913998ca8f564b165ffc9a73e4be174785
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-25 13:46:30 +0500

    change icon size to parent font size in right-arrow-icon, add props to change icon size by custom value

--- src/atomic/atom/right-arrow-icon.tsx
+++ src/atomic/atom/right-arrow-icon.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { SVGProps } from "react";
 
-export default function RightArrowIcon() {
+export default function RightArrowIcon(props: SVGProps<SVGSVGElement>) {
     return (
         <svg
             aria-label="right arrow icon"
-            width="19"
-            height="9"
+            width="1em"
+            height="1em"
             viewBox="0 0 19 9"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path d="m15.172.965 3.182 3.182a.5.5 0 0 1 0 .707l-3.182 3.182a.5.5 0 1 1-.708-.707L16.793 5H1a.5.5 0 0 1 0-1h15.793l-2.329-2.328a.5.5 0 1 1 .708-.707Z" />
         </svg>
