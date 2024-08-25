6c6880aae1d20c86ece08cb1ecac3a33fb734d3e
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-25 19:59:51 +0500

    change css to bootstrap classes

--- src/atomic/molecule/text-link-arrow.tsx
+++ src/atomic/molecule/text-link-arrow.tsx
@@ -9,7 +9,12 @@ interface IReviewLinkProps {
 
 export default function TextLinkArrow({ rightIcon, text, href }: IReviewLinkProps) {
     return (
-        <a href={href} target="_blank" rel="noopener noreferrer" className="text-link-arrow">
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-link-arrow d-flex align-items-center gap-2 text-decoration-none lh-sm"
+        >
             {text}
             {rightIcon}
         </a>
