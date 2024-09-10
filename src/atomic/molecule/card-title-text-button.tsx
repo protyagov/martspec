d1261018e54bc3da3c31f85b0f7bf70ec54b5205
renata tsedenova <recedenova@gmail.com>
2024-09-10 18:24:05 +0300

    [MS-1248] Keep working on this task, can’t figure out the styles

--- src/atomic/molecule/card-title-text-button.tsx
+++ src/atomic/molecule/card-title-text-button.tsx
@@ -2,41 +2,34 @@ import * as React from "react";
 
 interface Props {
     title: string;
-    textType: "p" | "ul"; 
-    textContent: string | string[]; 
+    textContent: string[]; // Должен быть массив строк для списка
     buttonText: string;
     buttonLink: string;
 }
 
 const CardTitleTextButton = ({
     title,
-    textType = "p", 
     textContent,
     buttonText,
     buttonLink
 }: Props): JSX.Element => {
     return (
-        <section>
-            <div className="row col-lg-6">
-                <div>
-                    <h2>{title}</h2>
-                    {textType === "ul" && Array.isArray(textContent) ? (
-                        <ul>
-                            {textContent.map((item, index) => (
-                                <li key={index} className="mb-4">{item}</li>
-                            ))}
-                        </ul>
-                    ) : textType === "p" && typeof textContent === "string" ? (
-                        <p>{textContent}</p>
-                    ) : null}
-                    <div className="btn-container">
-                        <a href={buttonLink} className="btn">
-                            {buttonText}
-                        </a>
-                    </div>
-                </div>
+        <div className="card-title-text-button">
+            <div className="">
+            <h2>{title}</h2>
+            <ul>
+                {textContent.map((item, index) => (
+                    <li key={index} className="mb-4">{item}</li>
+                ))}
+            </ul>
+            <div className="btn-container">
+                <a href={buttonLink} className="btn">
+                    {buttonText}
+                </a>
             </div>
-        </section>
+            </div>
+            
+        </div>
     );
 };
 
--- src/atomic/page/electrolyte.tsx
+++ src/atomic/page/electrolyte.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import _, { Locale } from "@/i18n/locale";
 import { Footer } from "@/atomic/organism/footer";
+import ImageI18N from "../atom/img-i18n";
 import NavigationBar from "@/atomic/organism/navbar";
 import PageHeader from "@/atomic/molecule/page-header";
 import Header from "@/atomic/organism/header";
@@ -11,7 +12,8 @@ export default function Electrolyte() {
         <>
             <NavigationBar />
 
-            <div className="ms-base-page ms-base-new electrolyte">
+        <div className="electrolyte">
+            <div className="ms-base-page ms-base-new">
                 <Header
                     title={_("ELECTROLYTE.HEAD")}
                     appId={1519596234}
@@ -30,83 +32,105 @@ export default function Electrolyte() {
                 />
             </div>
 
-            <CardTitleTextButton title={_("ELECTROLYTE.HEAD_2")}
-            textType = "p"
-            textContent={_("ELECTROLYTE.DESC2_1")}
-            buttonText={_("ELECTROLYTE.BTN_MORE")}
-            buttonLink={Locale.i18nLink(`vitamin/zinc`)} />
-
-            <div className="ms-base-page">
+            <div className="ms-base-new ms-base-page">
                 <section>
-                    <h4>{_("ELECTROLYTE.HEAD1")}</h4>
-                        <p className="mt-3">
-                            {_("ELECTROLYTE.DESC1")}
-                            <a href="https://ods.od.nih.gov/factsheets/Magnesium-HealthProfessional" target="_blank">
-                                <small>
-                                    <i>{_("BTN.SRC1")}.</i>
-                                </small>
-                            </a>
-                        </p>
-                </section>
-
-                <section>
-                    <h4>{_("ELECTROLYTE.HEAD2")}</h4>
-                    <p className="mt-3">
-                        {_("ELECTROLYTE.DESC2")}
-                        <a href="https://ods.od.nih.gov/factsheets/Calcium-Consumer" target="_blank">
-                            <small>
-                                <i>{_("BTN.FACT")}.</i>
-                            </small>
-                        </a>
-                    </p>
-                </section>
-
-                <section>
-                    <h4>{_("ELECTROLYTE.HEAD3")}</h4>
-                    <p className="mt-3">
-                        {_("ELECTROLYTE.DESC3")}
-                        <a href="https://ods.od.nih.gov/factsheets/Potassium-HealthProfessional" target="_blank">
-                            <small>
-                                <i>{_("BTN.SRC2")}.</i>
-                            </small>
-                        </a>
-                    </p>
-                </section>
-
-                <section>
-                    <h4>{_("ELECTROLYTE.HEAD4")}</h4>
-                    <p className="mt-3">
-                        {_("ELECTROLYTE.DESC4")}
-                        <a href="https://ods.od.nih.gov/factsheets/Iron-HealthProfessional" target="_blank">
-                            <small>
-                                <i>{_("BTN.FACT")}.</i>
-                            </small>
-                        </a>
-                    </p>
-                </section>
-
-                <section>
-                    <h4 style={{ color: "#BF4803" }}>{_("ELECTROLYTE.HEAD5")}</h4>
-                    <p className="mt-3">
-                        {_("ELECTROLYTE.DESC5")}
-                        <a href="https://en.wikipedia.org/wiki/Electrolyte_imbalance" target="_blank">
-                            <small>
-                                <i>{_("BTN.WIKI")}.</i>
-                            </small>
-                        </a>
-                    </p>
+                    <div className="row">
+                        <div className="col">
+                            <ImageI18N
+                                src="/img/page/electrolyte/electrolyte-middle-pic-ru.webp"
+                                w={487}
+                                h={515}
+                                cls="ms-base-image mt-mob-xs mb-6"
+                                alt={_("VITAMIN.ALT2")}
+                            />
+                        </div>
+                        <div className="col-lg-6">
+                            <CardTitleTextButton 
+                                title={_("ELECTROLYTE.HEAD_2")}
+                                textContent={[
+                                    _("ELECTROLYTE.DESC_1"),
+                                    _("ELECTROLYTE.DESC_2")
+                                ]}
+                                buttonText={_("ELECTROLYTE.BTN_MORE")}
+                                buttonLink={Locale.i18nLink(`vitamin/zinc`)} 
+                            />
+                        </div>
+                    </div>
                 </section>
             </div>
+        </div>
 
-            <div className="container mb-5 pb-5 text-center">
-                <img
-                    src="/img/page/electrolyte/electrolyte-app.webp"
-                    alt={_("MAIN.P1.IMG")}
-                    className="ms-base-image"
-                    width="512px"
-                    height="512px"
-                />
-            </div>
+        <div className="ms-base-page">
+            <section>
+                <h4>{_("ELECTROLYTE.HEAD1")}</h4>
+                    <p className="mt-3">
+                        {_("ELECTROLYTE.DESC1")}
+                        <a href="https://ods.od.nih.gov/factsheets/Magnesium-HealthProfessional" target="_blank">
+                            <small>
+                                <i>{_("BTN.SRC1")}.</i>
+                            </small>
+                        </a>
+                    </p>
+            </section>
+
+            <section>
+                <h4>{_("ELECTROLYTE.HEAD2")}</h4>
+                <p className="mt-3">
+                    {_("ELECTROLYTE.DESC2")}
+                    <a href="https://ods.od.nih.gov/factsheets/Calcium-Consumer" target="_blank">
+                        <small>
+                            <i>{_("BTN.FACT")}.</i>
+                        </small>
+                    </a>
+                </p>
+            </section>
+
+            <section>
+                <h4>{_("ELECTROLYTE.HEAD3")}</h4>
+                <p className="mt-3">
+                    {_("ELECTROLYTE.DESC3")}
+                    <a href="https://ods.od.nih.gov/factsheets/Potassium-HealthProfessional" target="_blank">
+                        <small>
+                            <i>{_("BTN.SRC2")}.</i>
+                        </small>
+                    </a>
+                </p>
+            </section>
+
+            <section>
+                <h4>{_("ELECTROLYTE.HEAD4")}</h4>
+                <p className="mt-3">
+                    {_("ELECTROLYTE.DESC4")}
+                    <a href="https://ods.od.nih.gov/factsheets/Iron-HealthProfessional" target="_blank">
+                        <small>
+                            <i>{_("BTN.FACT")}.</i>
+                        </small>
+                    </a>
+                </p>
+            </section>
+
+            <section>
+                <h4 style={{ color: "#BF4803" }}>{_("ELECTROLYTE.HEAD5")}</h4>
+                <p className="mt-3">
+                    {_("ELECTROLYTE.DESC5")}
+                    <a href="https://en.wikipedia.org/wiki/Electrolyte_imbalance" target="_blank">
+                        <small>
+                            <i>{_("BTN.WIKI")}.</i>
+                        </small>
+                    </a>
+                </p>
+            </section>
+        </div>
+
+        <div className="container mb-5 pb-5 text-center">
+            <img
+                src="/img/page/electrolyte/electrolyte-app.webp"
+                alt={_("MAIN.P1.IMG")}
+                className="ms-base-image"
+                width="512px"
+                height="512px"
+            />
+        </div>
 
             <Footer />
         </>
