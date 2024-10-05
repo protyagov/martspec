23b1148a0863a1dc17681b793f5b5a1abb13aa33
EGRrqq <egrrqqdev@gmail.com>
2024-10-05 12:50:41 -0400

    [MS-1262] remove test ladle story

--- src/stories/CardTitleText.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { Story } from "@ladle/react";
-import CardTitleText from "@/atomic/molecule/card-title-text";
-
-export const CardTitleText_Story: Story = () => (
-    <CardTitleText title="STORY PREVIEW" text="TEXT" bgColor="#E5F4D9" textColor="#151515" />
-);
