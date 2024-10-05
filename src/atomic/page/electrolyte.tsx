5d7fe13d300e77f75c7cee0fd2b765a137177c31
Alexandra Paramonova <alpar.wb@gmail.com>
2024-10-05 22:39:58 +0500

    [MS-1291] remove excess closing div tag on electrolyte page

--- src/atomic/page/electrolyte.tsx
+++ src/atomic/page/electrolyte.tsx
@@ -136,7 +136,6 @@ export default function Electrolyte() {
               </div>
             </div>
           </section>
-        </div>
 
                     <Review
                         codes={{
