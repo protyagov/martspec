0abac4dd5e5cf1b214e111b530c05f463c6fe481
EGRrqq <egrrqqdev@gmail.com>
2024-10-15 22:56:45 -0400

    [MS-1298] isolate build static logic into separate function
    - add err handling
    
    [MS-1298] remove duplication in dev server

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,4 +1,7 @@
+/** @type {string} */
+export const ladleBuildPath = "build_ladle";
+
 /** @type {import('@ladle/react').UserConfig} */
 export default {
-    outDir: "build_ladle",
+    outDir: ladleBuildPath,
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import fs from "fs";
 import fsExtra from "fs-extra";
+import { ladleBuildPath } from "./.ladle/config.mjs";
 
 export default defineConfig({
     plugins: [
@@ -14,14 +15,12 @@ export default defineConfig({
             configureServer(server) {
                 server.middlewares.use((req, res, next) => {
                     const { url } = req;
-
                     // Check if url is defined
                     if (!url) {
                         res.statusCode = 400; // Bad Request
                         res.end("Bad Request: URL is undefined");
                         return;
                     }
-
                     // Serve i18n files
                     if (url.startsWith("/i18n/")) {
                         const filePath = path.resolve(__dirname, "src/i18n", url.slice(6));
@@ -35,14 +34,13 @@ export default defineConfig({
                             return;
                         }
                     }
-
                     // Serve image files
                     if (url.startsWith("/img/")) {
                         const filePath = path.resolve(__dirname, "src/img", url.slice(5));
                         if (fs.existsSync(filePath)) {
                             // Determine content type based on file extension
                             const ext = path.extname(filePath).toLowerCase();
-                            let contentType = "application/octet-stream"; // Default content type
+                            let contentType: string;
 
                             switch (ext) {
                                 case ".jpg":
@@ -63,9 +61,9 @@ export default defineConfig({
                                     break;
                                 // Add more cases for other image types if needed
                                 default:
-                                    contentType = "application/octet-stream"; // Fallback for unknown types
+                                    // Fallback for unknown types
+                                    contentType = "application/octet-stream";
                             }
-
                             res.setHeader("Content-Type", contentType);
                             fs.createReadStream(filePath).pipe(res);
                             return;
@@ -75,20 +73,11 @@ export default defineConfig({
                             return;
                         }
                     }
-
                     next();
                 });
             },
-            writeBundle() {
-                // Copy i18n files
-                const i18nSrc = path.resolve(__dirname, "src/i18n");
-                const i18nDest = path.resolve(__dirname, "build_ladle/i18n");
-                fsExtra.copySync(i18nSrc, i18nDest);
-
-                // Copy image files
-                const imgSrc = path.resolve(__dirname, "src/img");
-                const imgDest = path.resolve(__dirname, "build_ladle/img");
-                fsExtra.copySync(imgSrc, imgDest);
+            async writeBundle() {
+                await copyStaticFiles(ladleBuildPath);
             },
         },
     ],
@@ -99,3 +88,30 @@ export default defineConfig({
         },
     },
 });
+
+async function copyStaticFiles(toPath: string) {
+    const staticFilesMap = [
+        { from: "src/i18n", to: `${toPath}/i18n` },
+        { from: "src/img", to: `${toPath}/img` },
+    ];
+
+    try {
+        await Promise.all(staticFilesMap.map(({ from, to }) => moveStatic(from, to)));
+        console.log("Successfully copied all static files");
+    } catch (error) {
+        console.error("Failed to copy static files:", error.message);
+        throw error;
+    }
+}
+async function moveStatic(fromPath: string, toPath: string) {
+    const from = path.resolve(__dirname, fromPath);
+    const to = path.resolve(__dirname, toPath);
+
+    try {
+        await fsExtra.copy(from, to);
+        console.log(`Successfully copied ${fromPath} to ${toPath}`);
+    } catch (error) {
+        console.error(`Failed to copy ${fromPath} to ${toPath}:`, error.message);
+        throw error;
+    }
+}
