5f11b8c03da427118928e03c7c63deb018549211
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2024-11-03 23:20:09 +0500

    [MS-1313] add ButtonApple ladle story

---
+++ src/stories/atom/button-apple.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import type { Story, StoryDefault } from "@ladle/react";
+import appIds from "@/data/app-ids.json";
+
+import ButtonApple from "@/atomic/atom/button-apple";
+
+export default {
+    title: "Atom",
+} satisfies StoryDefault;
+
+export const ButtonAppleStory: Story = () => (
+    <ButtonApple
+        appId={appIds["vitamin"]}
+        appDownloadTitle={'test'}
+    />
+);
+
+ButtonAppleStory.storyName = "ButtonApple";
