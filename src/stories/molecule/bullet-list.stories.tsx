b0fad33ffced2bcbf8af3ec231a27e4a09176a2e
Yaroslav <ravlushevich.iarek@mail.ru>
2024-11-06 12:08:28 +0300

    [MS-1323] Ladle: add BulletList story

---
+++ src/stories/molecule/bullet-list.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import type { Story, StoryDefault } from "@ladle/react";
+import BulletList from "@/atomic/molecule/bullet-list";
+import _ from "@/i18n/locale";
+
+export default {
+    title: "Molecule",
+} satisfies StoryDefault;
+
+export const BulletListStory: Story<{
+		items: string[];
+		iconColor: string;
+		className?: string;
+}> = (props) => (
+		<BulletList {...props} />
+);
+
+BulletListStory.args = {
+		items: [
+				_("EMOTION.LIST4.LI1_TEXT"),
+				_("EMOTION.LIST4.LI2_TEXT"),
+				_("EMOTION.LIST4.LI3_TEXT"),
+				_("EMOTION.LIST4.LI4_TEXT"),
+				_("EMOTION.LIST4.LI5_TEXT"),
+				_("EMOTION.LIST4.LI6_TEXT"),
+		],
+		iconColor: "#7B62FE",
+		className: "bullet-list"
+};
+
+BulletListStory.storyName = "BulletList";
