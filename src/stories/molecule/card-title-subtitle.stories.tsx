e4363cb34713be27a973791bc4e7fc2670468d11
Yaroslav <ravlushevich.iarek@mail.ru>
2024-11-11 20:08:02 +0300

    [MS-1324] add wrapper to component and and and fixed indent size

--- src/stories/molecule/card-title-subtitle.stories.tsx
+++ src/stories/molecule/card-title-subtitle.stories.tsx
@@ -8,20 +8,28 @@ export default {
 } satisfies StoryDefault;
 
 export const CardTitleSubtitleStory: Story<CardTitleSubtitleProps> = (props) => (
-		<CardTitleSubtitle {...props} />
+    <div className="row row-cols-xl-2 row-cols-1 mb-0 p-0 g-4">
+        <div className="col px-0">
+            <ul className="row row-cols-2 p-0 g-4">
+                <li key={props.title} className="col">
+                    <CardTitleSubtitle {...props} />
+                </li>
+            </ul>
+        </div>
+    </div>
 );
 
 CardTitleSubtitleStory.args = {
-		title: _("VITAMIN.LIST5.LI1_HEAD"),
-		subtitle: _("VITAMIN.LIST5.LI1_TEXT"),
-		bgImg: {
-				src: "/img/page/vitamin/vitamin-track-fat-soluble-bg.svg",
-				widthPerc: "90%",
-				alignX: "start",
-				alignY: "start",
-				translateX: "-20%",
-				translateY: "-22%",
-		}
+    title: _("VITAMIN.LIST5.LI1_HEAD"),
+    subtitle: _("VITAMIN.LIST5.LI1_TEXT"),
+    bgImg: {
+        src: "/img/page/vitamin/vitamin-track-fat-soluble-bg.svg",
+        widthPerc: "90%",
+        alignX: "start",
+        alignY: "start",
+        translateX: "-20%",
+        translateY: "-22%",
+    },
 };
 
 CardTitleSubtitleStory.storyName = "CardTitleSubtitle";
