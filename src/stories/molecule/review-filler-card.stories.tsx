bb03a1ee4d0f7b16f7670eec55dd9d9e346d9931
Yaroslav <ravlushevich.iarek@mail.ru>
2024-11-13 15:26:33 +0300

    [MS-1340] Ladle: add eviewFillerCard story

---
+++ src/stories/molecule/review-filler-card.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import type { Story, StoryDefault } from "@ladle/react";
+import _ from "@/i18n/locale";
+import appIds from "@/data/app-ids.json";
+
+import ReviewFillerCard from "@/atomic/molecule/review-filler-card";
+import { ReviewContext } from "@/atomic/molecule/review-context";
+import { SendReviewsLink } from "@/atomic/organism/review-link";
+
+export default {
+    title: "Molecule",
+} satisfies StoryDefault;
+
+export const ReviewFillerCardStory: Story = () => (
+    <ReviewContext.Provider
+        value={{
+            data: {
+                reviews: [],
+                appId: appIds["electrolyte"],
+                countryCode: "ru",
+            },
+            text: {
+                head: _("REVIEW.HEAD"),
+                description: _("REVIEW.DESCRIPTION"),
+                link: _("REVIEW.LINK_ALL_REVIEWS"),
+                fillerCard: {
+                    head: [_("REVIEW.FILLER_CARD.HEAD1"), _("REVIEW.FILLER_CARD.HEAD2"), _("REVIEW.FILLER_CARD.HEAD3")],
+                    link: _("REVIEW.FILLER_CARD.LINK"),
+                },
+            },
+        }}
+    >
+        <div className="row review py-5">
+            <ul className="review__list" style={{ maxWidth: "378px" }}>
+                <ReviewFillerCard link={<SendReviewsLink />} posIndex={1} key={_("REVIEW.FILLER_CARD.HEAD2")} />
+            </ul>
+        </div>
+    </ReviewContext.Provider>
+);
+
+ReviewFillerCardStory.storyName = "ReviewFillerCard";
