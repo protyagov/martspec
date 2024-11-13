8688b52662daad0cfd33138d38b8933cee203190
Yaroslav <ravlushevich.iarek@mail.ru>
2024-11-13 16:40:15 +0300

    [MS-1344] Ladle: add ReviewHead story

---
+++ src/stories/molecule/review-head.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import type { Story, StoryDefault } from "@ladle/react";
+import _ from "@/i18n/locale";
+import appIds from "@/data/app-ids.json";
+
+import ReviewHead from "@/atomic/molecule/review-head";
+import { ReviewContext } from "@/atomic/molecule/review-context";
+
+export default {
+    title: "Molecule",
+} satisfies StoryDefault;
+
+export const ReviewHeadStory: Story = () => (
+    <ReviewContext.Provider
+        value={{
+            data: {
+                reviews: [],
+                appId: appIds["electrolyte"],
+                countryCode: "ru",
+            },
+            text: {
+                head: _("REVIEW.HEAD"),
+                description: _("REVIEW.DESCRIPTION"),
+                link: _("REVIEW.LINK_ALL_REVIEWS"),
+                fillerCard: {
+                    head: [_("REVIEW.FILLER_CARD.HEAD1"), _("REVIEW.FILLER_CARD.HEAD2"), _("REVIEW.FILLER_CARD.HEAD3")],
+                    link: _("REVIEW.FILLER_CARD.LINK"),
+                },
+            },
+        }}
+    >
+        <ReviewHead />
+    </ReviewContext.Provider>
+);
+
+ReviewHeadStory.storyName = "ReviewHead";
