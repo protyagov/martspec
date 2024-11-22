545f0b3aeeb4f2892b07d656b4ce0187358f0b93
renata tsedenova <recedenova@gmail.com>
2024-11-22 13:33:46 +0400

    Updates

---
+++ development/assets/star-icon.stories-B6VUrWpV.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CpOiZ91u.js";import{S as e}from"./star-icon-b8OI9C4F.js";import{I as r}from"./decorators-BCvaFgqq.js";const m={title:"Atom"},t=()=>o.jsx(e,{});t.decorators=[r];t.storyName="StarIcon";typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{t as StarIconStory,m as default};
