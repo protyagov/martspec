8c1e35fe24bfdd9716b1d23520169616b5273c6f
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2024-11-25 21:31:27 +0500

    Updates

--- development/assets/review-description-B_Q6KJFA.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CqCq3haF.js";import{u as i}from"./review-context-kNimXcOa.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-DOPPkT9j.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CqCq3haF.js";import{u as o}from"./review-context-kNimXcOa.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
