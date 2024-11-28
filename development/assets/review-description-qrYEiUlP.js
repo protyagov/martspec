509c218df69fe2fa67ecc2ce8a1e7591e1b0a428
renata tsedenova <recedenova@gmail.com>
2024-11-28 20:46:58 +0400

    Updates

--- development/assets/review-description-qrYEiUlP.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DD50Fehh.js";import{u as i}from"./review-context-DxH_ADbN.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
