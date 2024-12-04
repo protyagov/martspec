51bff0367c8061a81400cc81cc26905dedaf1be8
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2024-12-04 12:32:19 +0500

    feat: delete VitaminItem component and all related imports and styles

--- src/atomic/page/vitamin.tsx
+++ src/atomic/page/vitamin.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import _, { Locale } from "@/i18n/locale";
 import { Footer } from "@/atomic/organism/footer";
 import NavigationBar from "@/atomic/organism/navbar";
-import VitaminItem from "@/atomic/molecule/vitamin-item";
 import PageHeader from "@/atomic/molecule/page-header";
 import ImageI18N from "@/atomic/atom/img-i18n";
 import CallToAction from "@/atomic/organism/call-to-action";
