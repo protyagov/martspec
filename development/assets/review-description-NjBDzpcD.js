4d133aafe7059a43d7bc8a21ad58b21408085f83
sokolovanadezhda31 <sokolova.nadezhda.31@gmail.com>
2024-12-09 14:14:05 +0200

    Updates

---
+++ development/assets/review-description-NjBDzpcD.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CRWFH9St.js";import{u as i}from"./review-context-ya2Rtn8O.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-Cge96Uib.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D71Ixqi8.js";import{u as o}from"./review-context-CdZr3eof.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
