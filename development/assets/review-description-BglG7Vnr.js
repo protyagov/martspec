f4589d99fc07fb39f321925ee3e646fe38bcec64
renata tsedenova <recedenova@gmail.com>
2024-12-13 14:40:22 +0400

    Updates

--- development/assets/review-description-BglG7Vnr.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-9Arkqoy1.js";import{u as i}from"./review-context-DtM1xrop.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-C1xMfoku.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-9Arkqoy1.js";import{u as o}from"./review-context-DtM1xrop.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
