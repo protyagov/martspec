cca0f5bc5d13d1486a7cef0be89de0543c9f4dcd
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2024-12-15 21:37:14 +0500

    feat: add ButtonChevronRight component, add story to ladle, add necessary styles

---
+++ src/stories/atom/button-chevron-right.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import _, { Locale } from "@/i18n/locale";
+import type { Story, StoryDefault } from "@ladle/react";
+
+import ButtonChevronRight from "@/atomic/atom/button-chevron-right";
+import { PageWrapper } from ".ladle/decorators";
+
+export default {
+    title: "Atom",
+} satisfies StoryDefault;
+
+export const ButtonChevronRightStory: Story<{
+    text: string;
+    link: string;
+    className?: string;
+}> = (props) => (
+    <div className="btn-chevron-right">
+        <ButtonChevronRight {...props} />
+    </div>
+);
+
+ButtonChevronRightStory.args = {
+    text: _("ELECTROLYTE.BTN_MORE"),
+    link: Locale.i18nLink(`coming-soon`),
+    className: "",
+};
+
+ButtonChevronRightStory.decorators = [PageWrapper];
+ButtonChevronRightStory.storyName = "ButtonChevronRight";
