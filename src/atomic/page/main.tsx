fe8d8a94ea9c2c40e68159f4cb6c33659c3951f6
renata tsedenova <recedenova@gmail.com>
2024-12-29 15:17:28 +0400

    [MS-1378] ll continue doing cards

--- src/atomic/page/main.tsx
+++ src/atomic/page/main.tsx
@@ -3,9 +3,11 @@ import _, { Locale } from "@/i18n/locale";
 import { Footer } from "@/atomic/organism/footer";
 import NavigationBar from "@/atomic/organism/navbar";
 import ImageI18N from "@/atomic/atom/img-i18n";
-import ScrollButton from "../atom/scroll-button";
+import ScrollButton from "@/atomic/atom/scroll-button";
 import appIds from "@/data/app-ids.json";
 import Review from "@/atomic/prototype/review";
+import CardAppSmall from "@/atomic/molecule/card-app-small";
+import CardAppLarge from "@/atomic/molecule/card-app-large";
 
 const getRandomArbitrary = (min: number, max: number) => Math.floor(Math.random() * (max + 1 - min) + min);
 const allAppIds = Object.values(appIds);
@@ -40,263 +42,39 @@ export default function LandingPage() {
                 </div>
             </section>
 
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/vitamin/logo-vitamin.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("VITAMIN.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("VITAMIN.HEAD")}</h1>
-                                    <p>{_("VITAMIN.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/vitamin/vitamin-home-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 img-fluid"
-                                    alt={_("VITAMIN.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("VITAMIN.TEXT")}</h4>
-                                <a href={Locale.i18nLink("vitamin")} className="mt-3 ms-btn-large">
-                                    {_("VITAMIN.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+            <section className="ms-s-offset">
+                <div className="mb-5">
+                    <CardAppLarge
+                        backgroundColor="#F9F9FF"
+                        icon={<img width={70} height={70} src="/img/page/vitamin/logo-vitamin.svg" alt={_("VITAMIN.ICO")} />}
+                        title={_("VITAMIN.HEAD")}
+                        subtitle={_("VITAMIN.SUBHEAD")}
+                        content={_("VITAMIN.TEXT")}
+                        button={{
+                        text: _("CAREERS.BUTTON3"),
+                        link: "",
+                        color: "#1686FF",
+                        }}
+                        imageSrc="/img/page/vitamin/vitamin-app-main-page.webp"
+                        imageAlt={_("VITAMIN.IMG")}
+                        useI18NImage={true}
+                        isImageOnLeft={false}
+                    />
                 </div>
-            </section>
-
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/body-size/logo-size.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("SIZE.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("SIZE.HEAD")}</h1>
-                                    <p>{_("SIZE.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/body-size/size-home-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 img-fluid"
-                                    alt={_("SIZE.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("SIZE.TEXT")}</h4>
-                                <a href={Locale.i18nLink("bodysize")} className="mt-3 ms-btn-large">
-                                    {_("SIZE.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/electrolyte/logo-electrolyte.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("ELECTROLYTE.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("ELECTROLYTE.HEAD")}</h1>
-                                    <p>{_("ELECTROLYTE.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/electrolyte/electrolyte-home.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 img-fluid"
-                                    alt={_("ELECTROLYTE.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("ELECTROLYTE.TEXT")}</h4>
-                                <a href={Locale.i18nLink("electrolyte")} className="mt-3 ms-btn-large">
-                                    {_("ELECTROLYTE.BTN_MORE")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/body-mass/logo-mass.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("MASS.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("MASS.HEAD")}</h1>
-                                    <p>{_("MASS.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/body-mass/mass-home-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 px-lg-4 px-0 img-fluid"
-                                    alt={_("MASS.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("MASS.TEXT")}</h4>
-                                <a href={Locale.i18nLink("bodymass")} className="mt-3 ms-btn-large">
-                                    {_("MASS.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/zinc/logo-zinc.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("ZINC.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("ZINC.HEAD")}</h1>
-                                    <p>{_("ZINC.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                {/* <ImageI18N src="/img/p_zinc.512.webp" w={512} h={512} cls="mx-auto my-4 img-fluid" alt={_("ZINC.IMG")} /> */}
-                                <ImageI18N
-                                    src="/img/page/zinc/zinc-app-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 img-fluid"
-                                    alt={_("ZINC.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("ZINC.TEXT")}</h4>
-                                <a href={Locale.i18nLink("bodyzinc")} className="mt-3 ms-btn-large">
-                                    {_("ZINC.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="ms-s-product my-5">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/waistline/logo-waist.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("WAIST.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("WAIST.HEAD")}</h1>
-                                    <p>{_("WAIST.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/waistline/waist-home-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 px-lg-4 px-0 img-fluid"
-                                    alt={_("WAIST.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("WAIST.TEXT")}</h4>
-                                <a href={Locale.i18nLink("waistline")} className="mt-3 ms-btn-large">
-                                    {_("WAIST.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="ms-s-product">
-                <div className="ms-s-offset">
-                    <div className="row">
-                        <div className="col p-container">
-                            <div className="d-flex p-head">
-                                <img
-                                    src="/img/page/emotion/logo-emotion.svg"
-                                    className="img-fluid me-3"
-                                    height={48}
-                                    width={48}
-                                    alt={_("EMOTION.ICO")}
-                                />
-                                <div>
-                                    <h1>{_("EMOTION.HEAD")}</h1>
-                                    <p>{_("EMOTION.SUBHEAD")}</p>
-                                </div>
-                            </div>
-                            <div className="p-img">
-                                <ImageI18N
-                                    src="/img/page/emotion/emotion-home-en.webp"
-                                    w={512}
-                                    h={512}
-                                    cls="mx-auto my-4 img-fluid"
-                                    alt={_("EMOTION.IMG")}
-                                />
-                            </div>
-                            <div className="p-text">
-                                <h4>{_("EMOTION.TEXT")}</h4>
-                                <a href={Locale.i18nLink("emotion")} className="mt-3 ms-btn-large">
-                                    {_("EMOTION.BTN_OPEN")}
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                
+                <div className="col-4">
+                    <CardAppSmall
+                        backgroundColor="#F9F9FF"
+                        icon={<img width={70} height={70} src="/img/page/vitamin/vitamin-ring-icon-main-page.svg" alt={_("VITAMIN.ICO")} />}
+                        title={_("VITAMIN.HEAD")}
+                        subtitle={_("VITAMIN.SUBHEAD")}
+                        content={_("VITAMIN.TEXT")}
+                        button={{
+                        text: _("CAREERS.BUTTON3"),
+                        link: "",
+                        color: "#1686FF",
+                        }}
+                    />
                 </div>
             </section>
 
