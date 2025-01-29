97e101e123d324b3662c82933e02b780c123d1f3
renata tsedenova <recedenova@gmail.com>
2025-01-29 19:50:45 +0400

    Updates

---
+++ development/assets/review-description-D08O8mGo.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CFOODtTj.js";import{u as i}from"./review-context-CoE-FMi6.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-DZ-wjOBe.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BnALUqQ7.js";import{u as o}from"./review-context-Qnx1pKdX.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
