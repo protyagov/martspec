db4884f5afc4520bb202e268b1a28e6997d2f8f2
renata tsedenova <recedenova@gmail.com>
2025-02-17 13:27:52 +0400

    Updates

---
+++ development/assets/review-description-DBJdmKA7.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fb3FDtH_.js";import{u as i}from"./review-context-DFFRYS_X.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-CaTxxcJj.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fb3FDtH_.js";import{u as o}from"./review-context-DFFRYS_X.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
