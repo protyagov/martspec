1a3bcbba61cf26981c27f99d78e7c4bb827a7593
renata tsedenova <recedenova@gmail.com>
2025-03-11 11:25:59 +0400

    Updates

---
+++ development/assets/review-filler-card-BfRTYCuP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-O65K3x0t.js";import{u as l}from"./review-context-DCcT73o4.js";function d({link:r,posIndex:a}){const{text:i}=l();return e.jsx("li",{className:"review-card filler-card",children:e.jsxs("div",{className:"filler-card__wrapper",children:[e.jsx("h3",{className:"filler-card__header",children:i.fillerCard.head[a]}),r]})})}export{d as R};
