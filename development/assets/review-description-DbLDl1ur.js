edd21024025e40a94fc4dedcaf3f9f9bbae4f7fe
Dmitry Chigrin <>
2025-03-23 02:19:56 +0500

    Updates

--- development/assets/review-description-DbLDl1ur.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-NRNKEc3P.js";import{u as i}from"./review-context-CmnrxyZ6.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
