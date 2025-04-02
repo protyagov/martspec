48813d283c04479cd85a2c83ae4c49d1c2392f9e
renata tsedenova <recedenova@gmail.com>
2025-04-02 13:41:09 +0400

    Updates

---
+++ development/assets/review-description-BAT_w1js.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1eOk-r2l.js";import{u as i}from"./review-context-CJzHS0ZQ.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
