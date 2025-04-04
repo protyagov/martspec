fe0709da044106e01c9c3e03b317d197cf6ad1e4
sokolovanadezhda31 <sokolova.nadezhda.31@gmail.com>
2025-04-04 19:56:07 +0300

    Updates

--- development/assets/review-filler-card-DNCn5YQc.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-1eOk-r2l.js";import{u as l}from"./review-context-CJzHS0ZQ.js";function d({link:r,posIndex:a}){const{text:i}=l();return e.jsx("li",{className:"review-card filler-card",children:e.jsxs("div",{className:"filler-card__wrapper",children:[e.jsx("h3",{className:"filler-card__header",children:i.fillerCard.head[a]}),r]})})}export{d as R};
--- development/assets/review-head-j4awC0zw.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-1eOk-r2l.js";import{u as o}from"./review-context-CJzHS0ZQ.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
