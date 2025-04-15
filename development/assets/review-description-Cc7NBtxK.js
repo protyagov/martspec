bf9ee565bc3e6d177c49957a04328a688f13b0f8
sokolovanadezhda31 <sokolova.nadezhda.31@gmail.com>
2025-04-15 15:05:35 +0300

    Updates

---
+++ development/assets/review-description-Cc7NBtxK.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BDSXUc98.js";import{u as i}from"./review-context-Ceut6abz.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-j-_Tkj-C.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BDSXUc98.js";import{u as o}from"./review-context-Ceut6abz.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
