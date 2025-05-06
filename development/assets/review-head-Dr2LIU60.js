70613222d0e337bbe2515d8fcbfe6eab3338b873
Dmitry Chigrin <>
2025-05-06 23:32:58 +0500

    Updates

--- development/assets/review-head-Dr2LIU60.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DGOEaaPP.js";import{u as o}from"./review-context-8A0ggBq0.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
---
+++ development/assets/review-head-bhRirHlE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-OwQ1Iije.js";import{u as o}from"./review-context-BdqPBt0U.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
