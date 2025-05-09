d35132b104b842356cef2153f69382d57fa29b95
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-05-09 11:51:07 -0300

    Updates

---
+++ development/assets/review-head-CKCJ5AVA.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ChWWPBWA.js";import{u as o}from"./review-context-Bk6JF_qE.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
--- development/assets/review-head-DGNGH_7D.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-lDpmRkWr.js";import{u as o}from"./review-context-BxnfWY9B.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
