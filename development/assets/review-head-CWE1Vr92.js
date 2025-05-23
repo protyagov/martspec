0d56f4a96b24bdca45217b1cd0a9b85c11b61fd1
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-05-23 23:20:50 +0300

    Updates

---
+++ development/assets/review-head-CWE1Vr92.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DY4-WWmU.js";import{u as o}from"./review-context-D1Fek3FX.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
--- development/assets/review-head-CtIETNAB.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-C7SyCscX.js";import{u as o}from"./review-context-DWj7LWC7.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
