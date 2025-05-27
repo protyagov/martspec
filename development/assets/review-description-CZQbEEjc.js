c3eb016aa7ccc0bb6ee28a1eb9785fe9fc9bc119
sokolovanadezhda31 <sokolova.nadezhda.31@gmail.com>
2025-05-27 13:54:11 +0300

    Updates

--- development/assets/review-description-CZQbEEjc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DY4-WWmU.js";import{u as i}from"./review-context-D1Fek3FX.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
