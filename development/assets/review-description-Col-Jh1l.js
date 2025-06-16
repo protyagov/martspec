9c422e2b0acdc20679df9809885db80af26690c4
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2025-06-16 23:51:53 +0300

    Updates

--- development/assets/review-description-Col-Jh1l.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-COf6YU3s.js";import{u as i}from"./review-context-DtKGjn0p.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
