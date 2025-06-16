8c330f092e86dd2db78c8bac558a71ad26e2b684
leyli <tr.leyli@gmail.com>
2025-06-17 02:00:20 +0300

    Updates

---
+++ development/assets/review-description-CgvRdRd2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CkbKrZNu.js";import{u as i}from"./review-context-83Y-WiQp.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
