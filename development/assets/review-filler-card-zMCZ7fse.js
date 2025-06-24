4565981f51c149d8a1be86c39cb8ef21cb30f5c2
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-06-25 01:07:51 +0300

    Updates

---
+++ development/assets/review-filler-card-zMCZ7fse.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DqsohgEt.js";import{u as l}from"./review-context-PRp_Y0kB.js";function d({link:r,posIndex:a}){const{text:i}=l();return e.jsx("li",{className:"review-card filler-card",children:e.jsxs("div",{className:"filler-card__wrapper",children:[e.jsx("h3",{className:"filler-card__header",children:i.fillerCard.head[a]}),r]})})}export{d as R};
--- development/assets/review-head-BiredagE.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BCjOj83s.js";import{u as o}from"./review-context-CCBRqSOU.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
