d7fd78a3a97f93c1dd3f38c1237c95482840b42a
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-06-27 22:13:45 +0300

    add translation title and description on all languages

--- src/atomic/page/magnesium.tsx
+++ src/atomic/page/magnesium.tsx
@@ -5,31 +5,31 @@ import Header from "@/atomic/organism/header";
 import NavigationBar from "@/atomic/organism/navbar";
 import { Footer } from "@/atomic/organism/footer";
 import ScrollButton from "../atom/scroll-button";
-//import '@/sass/page/magnesium.scss'
 
 export default function Magnesium() {
     const appId = getAppId();
     return (
         <>
             <NavigationBar />
-            <Header
-                title={_("MAGNESIUM.HEAD")}
-                appId={appId}
-                appDownloadTitle={_("MAGNESIUM.DWN")}
-                imgSrc="/img/page/magnesium/Img-MG-header-en.webp"
-                imgAlt={_("MAGNESIUM.IMG")}
-                imgH={305}
-                imgW={450}
-            >
-                <div className="magnesium">
-                    <ul className="header-content-list">
-                        <li>{_("MAGNESIUM.ABOUT_1")}</li>
-                        <li>{_("MAGNESIUM.ABOUT_2")}</li>
-                        <li>{_("MAGNESIUM.ABOUT_3")}</li>
-                    </ul>
-                </div>
-            </Header>
-
+            <div className="ms-base-page ms-base-new">
+                <Header
+                    title={_("MAGNESIUM.HEAD")}
+                    appId={appId}
+                    appDownloadTitle={_("MAGNESIUM.DWN")}
+                    imgSrc="/img/page/magnesium/Img-MG-header-en.webp"
+                    imgAlt={_("MAGNESIUM.IMG")}
+                    imgH={305}
+                    imgW={450}
+                >
+                    <div className="magnesium">
+                        <ul className="header-content-list">
+                            <li>{_("MAGNESIUM.ABOUT_1")}</li>
+                            <li>{_("MAGNESIUM.ABOUT_2")}</li>
+                            <li>{_("MAGNESIUM.ABOUT_3")}</li>
+                        </ul>
+                    </div>
+                </Header>
+            </div>
             <Footer />
             <ScrollButton />
         </>
