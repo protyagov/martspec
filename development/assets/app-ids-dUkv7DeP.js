d7a3436ac8a1cfeb8e1cdfbbba6ed60b19102c60
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-06-28 16:04:37 +0300

    Updates

--- development/assets/app-ids-dUkv7DeP.js
+++
@@ -1,1 +0,0 @@
-const o={vitamin:1519596234,emotion:1562956213,bodyzinc:1547395353,electrolyte:1507719251,waistline:1506250420,bodymass:1507588203,bodysize:1564205068,iron:6740498847};export{o as a};
---
+++ development/assets/review-head-DBDSGQ3Y.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D7GOF5_M.js";import{u as o}from"./review-context-qMwYY4lc.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
