5cbac9dd38540c2ef58bb7e8a815b34b866a26e7
leyli <tr.leyli@gmail.com>
2025-07-06 23:40:12 +0300

    big cards

--- src/atomic/molecule/team-Member.tsx
+++ src/atomic/molecule/team-Member.tsx
@@ -8,6 +8,7 @@ interface TeamMemberProps {
     avatar: string;
     link: string;
     text: string;
+    isBig?: boolean;
     // color: "white" | "black";
 }
 
@@ -32,6 +33,7 @@ export default function TeamMember(props: TeamMemberProps) {
             return (
                 <a href={_(props.link)} target="_blank" rel="noopener" aria-label="LinkedIn">
                     <svg {...svgAttrs}>
+                        
                         {/* <title>{_("TEAM." )}</title> */}
                         <path
                             id="Vector"
@@ -42,41 +44,77 @@ export default function TeamMember(props: TeamMemberProps) {
                 </a>
             );
         }
-        return <div className="itemLink"></div>;
+        return <div className="itemLink "></div>;
     }
 
+   
+    if (props.isBig) {
+            return (
+                <div className="col-12 col-lg-6 mb-4 d-flex">
+                    <div className="card bg-card rounded-5 flex-row flex-grow-1 h-100" >
+                        <div className="col-md-5 d-flex  p-3">
+                            <img 
+                                src={props.avatar}
+                                onError={(e) => (e.currentTarget.src = "/img/team_placeholder.png")}
+                                alt={_("TEAM." + props.title) + " " + props.name}
+                                className=" rounded-5  w-100"
+                                
+                            />
+                        </div>
+                        <div className="col-md-7 d-flex flex-column justify-content-center p-4">
+                            <h3 className="card-title text-muted  text-lg-start">{_("TEAM." + props.name)}</h3>
+                            <h5 className="card-subtitle text-muted  text-lg-start mb-3">{_("TEAM." + props.title)}</h5>
+                            {props.text && <p className="card-text  text-lg-start">{_(props.text)}</p>}
+                            <div className=" text-lg-start mt-3">
+                                <ItemLink />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
     return (
-        <div className="d-flex flex-column team-member">
-            <div className="team-members">
-                <ItemLink />
-                {/* <a href={_( props.link )} target="_blank"
-             rel="noopener"
-             aria-label="LinkedIn">
-            <svg {...svgAttrs}>
-                {/* <title>{_("TEAM." )}</title> */}
-                {/* <path id="Vector" d="M22 13.4906V22H17.2646V14.0094C17.2646 12.0377 16.5739 10.6887 14.8969 10.6887C13.6143 10.6887 12.8251 11.6226 12.5291 12.4529C12.4304 12.7641 12.3319 13.1792 12.3319 13.6981V22H7.59645C7.59645 22 7.69501 8.50944 7.59645 7.16044H12.3319V9.23577C12.9238 8.1981 14.1076 6.74528 16.5739 6.74528C19.6322 6.74528 22 8.92459 22 13.4906ZM2.66361 0C1.08519 0 0 1.14149 0 2.59432C0 4.04714 0.986481 5.18863 2.56505 5.18863C4.24219 5.18863 5.22867 4.04714 5.22867 2.59432C5.32737 1.03766 4.34075 0 2.66361 0ZM0.295974 22H5.0314V7.16044H0.295974V22Z" fill="white"/> */}
-                {/* </svg> */}
-                {/* </a> */}
-                <img
-                    src={props.avatar}
-                    onError={(e) => (e.currentTarget.src = "/img/team_placeholder.png")}
-                    alt={_("TEAM." + props.title) + " " + props.name}
-                    className="rounded-circle mb-2 p-0"
-                />
+        <div className="col mb-3">
+            <div className="card bg-card rounded-5 h-100 d-flex flex-column " >
+                <div className="p-3 pb-0 " >
+                    
+                    {/* <a href={_( props.link )} target="_blank"
+                rel="noopener"
+                aria-label="LinkedIn">
+                <svg {...svgAttrs}>
+                    {/* <title>{_("TEAM." )}</title> */}
+                    {/* <path id="Vector" d="M22 13.4906V22H17.2646V14.0094C17.2646 12.0377 16.5739 10.6887 14.8969 10.6887C13.6143 10.6887 12.8251 11.6226 12.5291 12.4529C12.4304 12.7641 12.3319 13.1792 12.3319 13.6981V22H7.59645C7.59645 22 7.69501 8.50944 7.59645 7.16044H12.3319V9.23577C12.9238 8.1981 14.1076 6.74528 16.5739 6.74528C19.6322 6.74528 22 8.92459 22 13.4906ZM2.66361 0C1.08519 0 0 1.14149 0 2.59432C0 4.04714 0.986481 5.18863 2.56505 5.18863C4.24219 5.18863 5.22867 4.04714 5.22867 2.59432C5.32737 1.03766 4.34075 0 2.66361 0ZM0.295974 22H5.0314V7.16044H0.295974V22Z" fill="white"/> */}
+                    {/* </svg> */}
+                    {/* </a> */}
+                    
+                    <img
+                        src={props.avatar}
+                        onError={(e) => (e.currentTarget.src = "/img/team_placeholder.png")}
+                        alt={_("TEAM." + props.title) + " " + props.name}
+                        className="   rounded-5"
+                        
+                        
+                    />
+                     
+                </div>
+                <div className="card-body ">
+                    <h4 className="card-title text-muted"> {_("TEAM." + props.name)} </h4>
+                    <h5 className="card-subtitle text-muted"> {_("TEAM." + props.title)} </h5>
+                <ItemLink/>            
+                </div>
+                
+                {/* <div */}
+                {/* // className="team-text" */}
+                {/* className={cn} */}
+                {/* > */}
+                {/* <title>{_("TEAM." + props.text )}</title> */}
 
-                <h4> {_("TEAM." + props.name)} </h4>
-                <h5> {_("TEAM." + props.title)} </h5>
-            </div>
-            {/* <div */}
-            {/* // className="team-text" */}
-            {/* className={cn} */}
-            {/* > */}
-            {/* <title>{_("TEAM." + props.text )}</title> */}
+                {/* <p>{ _(props.text) }</p> */}
+                {/* </div> */}
 
-            {/* <p>{ _(props.text) }</p> */}
-            {/* </div> */}
-
-            {/* <button className="team-btn" onClick={() => setIsActive(!isActive)}>подробнее</button> */}
+                {/* <button className="team-btn" onClick={() => setIsActive(!isActive)}>подробнее</button> */}
+            </div>  
         </div>
     );
 }
--- src/atomic/page/team.tsx
+++ src/atomic/page/team.tsx
@@ -1,23 +1,64 @@
 import * as React from "react";
+import _, { Locale } from "@/i18n/locale";
 import { Footer } from "@/atomic/organism/footer";
 import NavigationBar from "@/atomic/organism/navbar";
-import _ from "@/i18n/locale";
 import TeamMember from "@/atomic/molecule/team-Member";
-import teamData from "@/data/team.json";
+import BigTeamMember from "@/atomic/molecule/team-Member";
+import teamData from "@/data/team/team.json";
 import ScrollButton from "../atom/scroll-button";
+import Header from "@/atomic/organism/header";
+import ImageI18N from "@/atomic/atom/img-i18n";
+import CardTitleTextButton from "@/atomic/molecule/card-title-text-button";
 
 export default function Team() {
     return (
         <>
             <NavigationBar />
 
-            <div className="ms-base-page pb-5 text-center ms-team">
-
+            <div className="ms-base-page pb-5 ms-team row ms-team-info-section">
+                <Header
+                    title={_("TEAM.HEAD")}
+                >
+                    <p className="font-normal">{_("TEAM.ABOUT")}</p>
+                </Header>
+                <div className="">
+                        <div className="row justify-content-center">
+                            {teamData.slice(0, 2).map((member, index) => (
+                                <TeamMember 
+                                key={`big-${index}`} 
+                                {...member} 
+                                isBig={true} 
+                                />
+                            ))}
+                        </div>
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 justify-content-center ">
+                            {teamData.slice(2).map((member, index) => (
+                                <TeamMember key={`normal-${index}`} 
+                {...member}
+                />
+                            ))}
+                        </div>
+                    
+                </div>
                 <section>
-                    <div className=" d-flex flex-wrap justify-content-center members-list">
-                        {teamData.map((member) => (
-                            <TeamMember key={member.name} {...member} />
-                        ))}
+                    <div className="card-body row d-flex g-4 justify-content-between ">
+                        <div className="col-12 col-xxl-5 order-2 order-xxl-1">
+                            <ImageI18N
+                                src="/img/page/team/team_card.svg"
+                                w={720}
+                                h={315}
+                            />
+                        </div>
+                        <div className="row col-12 col-xxl-6 order-1 order-xxl-2 d-flex justify-content-between g-4">
+                            <CardTitleTextButton
+                                title={_("TEAM.HEAD1")}
+                                textContent={[_("TEAM.TEXT1_1")]}
+                                buttonText={_("CAREERS.BUTTON3")}
+                                buttonLink={Locale.i18nLink(`team`)}
+                                buttonColor="#1686FF"
+                                
+                            />
+                        </div>
                     </div>
                 </section>
             </div>
