dc77fe4340348510a8fe82c81509065e9bf75677
leilitr <tr.leyli@gmail.com>
2025-07-12 00:05:46 +0300

    Updates

---
+++ development/assets/review-description-zFAsocBK.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CzkKndLK.js";import{u as i}from"./review-context-Bcrof9K7.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-yvyULtrs.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CzkKndLK.js";import{u as o}from"./review-context-Bcrof9K7.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
