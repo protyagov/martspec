a8e19d2f0ea2ba5eca528704a2437a4fb4f3c8b9
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-07-28 23:17:45 +0300

    Updates

---
+++ development/assets/review-description-CDojKWp8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BZjDongT.js";import{u as i}from"./review-context-C8bz9Tu8.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
