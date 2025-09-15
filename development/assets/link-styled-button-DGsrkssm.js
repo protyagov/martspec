d5e25871314c93e47bb234d839ffbfaffe70c484
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2025-09-15 23:40:07 +0300

    Updates

---
+++ development/assets/link-styled-button-DGsrkssm.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-ub9H9GO7.js";const a=({text:r,link:e,color:o="#8079CC",textColor:n="#fff",className:t=""})=>s.jsx("a",{href:e,rel:"noopener noreferrer",className:`btn ${t}`,style:{backgroundColor:o,color:n},children:r});export{a as L};
