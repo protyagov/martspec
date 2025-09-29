1da95756cc986205136f0ab94acd33dcc761cce8
leyli <tr.leyli@gmail.com>
2025-09-29 18:44:02 +0300

    Updates

--- development/assets/review-description-DcsLsKfV.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D59do0H2.js";import{u as i}from"./review-context-ZdelT5BU.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-Bt6oTyxz.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Rok2_v23.js";import{u as o}from"./review-context-rNaIlrNa.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
