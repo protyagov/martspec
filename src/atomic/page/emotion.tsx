efcb0a5ef914915fe05f9f7605233ab373fb6ad7
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2025-10-20 23:42:45 +0300

    [MS-1451] added breadcrumbs to page emotion

--- src/atomic/page/emotion.tsx
+++ src/atomic/page/emotion.tsx
@@ -13,9 +13,12 @@ import Review from "@/atomic/prototype/review";
 import { getAppId } from "@/service/AppleService";
 import ScrollButton from "../atom/scroll-button";
 import CardTitleTextImageCustom from "@/atomic/molecule/card-title-text-image-custom";
+import { Breadcrumb } from "@/atomic/organism/breadcrumb";
+import { useBreadcrumbs } from "@/hooks/useBreadcrumbs"
 
 export default function Emotion() {
     const appId = getAppId();
+    const items = useBreadcrumbs();
     const advantages: Array<IconTitleText> = [
         {
             icon: {
@@ -56,6 +59,9 @@ export default function Emotion() {
             <NavigationBar />
 
             <div className="ms-base-page emotion">
+                <div className="row">
+                    <Breadcrumb items={items} />
+                </div>
                 <Header
                     title={_("EMOTION.HEAD")}
                     appId={appId}
