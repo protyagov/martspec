22069bae241f79105bbdeb56e5290b62aa78b931
leyli <tr.leyli@gmail.com>
2025-12-05 22:12:31 +0300

    Updates

---
+++ development/assets/review-description-B-p4434D.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BS9o9Xjm.js";import{u as i}from"./review-context-DHYuCi1L.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-DMO4LuKs.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BS9o9Xjm.js";import{u as o}from"./review-context-DHYuCi1L.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
