284a97e6d759a45b952ae39616e21504b87a3dc7
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2026-01-15 22:58:39 +0200

    Updates

--- development/assets/review-description-BRVW4Twj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DNqP0V2K.js";import{u as i}from"./review-context-cbx9lRyN.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
