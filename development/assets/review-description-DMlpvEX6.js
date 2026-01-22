790ae8689e5c79016a8813afa6f8622b64701b97
leyli <tr.leyli@gmail.com>
2026-01-22 18:04:27 +0300

    Updates

---
+++ development/assets/review-description-DMlpvEX6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-cf0lQdmA.js";import{u as i}from"./review-context-Cd_lMnZo.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
