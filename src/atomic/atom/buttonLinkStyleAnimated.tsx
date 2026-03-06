43175b7af4b386c59064947a15a6669ca4d01ced
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2026-03-06 19:01:05 +0200

    changed background-color, padding-top and padding-bottom

--- src/atomic/atom/buttonLinkStyleAnimated.tsx
+++ src/atomic/atom/buttonLinkStyleAnimated.tsx
@@ -7,7 +7,7 @@ interface AnimatedLinkButtonProps {
 
 const ButtonLinkStyleAnimated: React.FC<AnimatedLinkButtonProps> = ({ text, link }) => {
     return (
-        <a href={link} rel="noopener noreferrer" className="link-button-animated fs-4 rounded-5 py-2 px-5 mt-auto">
+        <a href={link} rel="noopener noreferrer" className="link-button-animated fs-4 rounded-5 px-5 mt-auto">
             {text}
         </a>
     );
--- src/atomic/atom/buttonLinkStylePlain.tsx
+++ src/atomic/atom/buttonLinkStylePlain.tsx
@@ -5,12 +5,12 @@ interface AnimatedLinkButtonProps {
     link: string;
 }
 
-const ButtonLinkStyleAnimated: React.FC<AnimatedLinkButtonProps> = ({ text, link }) => {
+const ButtonLinkStylePlain: React.FC<AnimatedLinkButtonProps> = ({ text, link }) => {
     return (
-        <a href={link} rel="noopener noreferrer" className="link-button-plain fs-4 rounded-5 py-2 px-5 mt-auto">
+        <a href={link} rel="noopener noreferrer" className="link-button-plain fs-4 rounded-5 px-5 mt-auto">
             {text}
         </a>
     );
 };
 
-export default ButtonLinkStyleAnimated;
+export default ButtonLinkStylePlain;
