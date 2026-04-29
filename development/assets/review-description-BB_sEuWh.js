0d65cec03aceac67cb38149ce42a273a95372827
leyli <tr.leyli@gmail.com>
2026-04-30 00:13:31 +0300

    Updates

--- development/assets/review-description-BB_sEuWh.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DBPcvZzY.js";import{u as i}from"./review-context-BFjHhTE3.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-BWPpZdHE.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DBPcvZzY.js";import{u as o}from"./review-context-BFjHhTE3.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
