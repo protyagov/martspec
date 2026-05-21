bc544f9d616a733e81222cf3c6e4b1333dde005b
Alex Protyagov <protyagov@gmail.com>
2026-05-21 00:27:21 -0500

    Updates

---
+++ development/assets/review-description-C4I8aj_t.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DbF0kCI8.js";import{u as i}from"./review-context-iQg8XHxb.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
