7d28e6235d2fa592e65fa60907e0c03d6dcf734f
Andrey <andreybogdanovich06@gmail.com>
2026-05-28 12:49:08 +0300

    [MS-1648] redesign 404 page, update translations

--- src/atomic/page/404.tsx
+++ src/atomic/page/404.tsx
@@ -1,22 +1,23 @@
 import * as React from "react";
 import _ from "@/i18n/locale";
+import NavigationBar from "@/atomic/organism/navbar";
+import ButtonLinkStyleAnimated from "../atom/buttonLinkStyleAnimated";
 
 export default function PageError_404() {
     return (
-        <div className="container-xl text-center pt-5 mt-5">
-            <div className="row justify-content-center pt-5 mb-4">
-                <div className="col-8 col-sm-6 col-lg-3">
-                    <img src="/img/error_404.svg" alt={_("ERROR.404_HEAD")} className="img-fluid mx-auto d-block mb-5" />
+        <>
+            <NavigationBar />
+            <div className="ms-base-page error-404 text-center px-3">
+                <div className="error-404-content">
+                    <div className="error-404-image-wrap">
+                        <img src="/img/error_404.svg" alt={_("ERROR.404_HEAD")} className="error-404-image" />
+                        <span className="error-404-number">404</span>
+                    </div>
+                    <h1>{_("ERROR.404_HEAD")}</h1>
+                    <p className="text-muted">{_("ERROR.404_TEXT")}</p>
                 </div>
+                <ButtonLinkStyleAnimated text={_("ERROR.404_BTN")} link="/" />
             </div>
-
-            <h1 className="my-5">{_("ERROR.404_HEAD")}</h1>
-
-            <p className="mb-5">{_("ERROR.404_TEXT")}</p>
-
-            <a href="/" className="btn btn-outline-secondary btn-lg">
-                {_("ERROR.404_BTN")}
-            </a>
-        </div>
+        </>
     );
 }
