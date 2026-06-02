82630974953e44353f63e447ee16622a8ef84baa
leyli <tr.leyli@gmail.com>
2026-06-02 22:47:30 +0300

    Updates

---
+++ development/assets/review-description-D046lMdx.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D7kHWa39.js";import{u as i}from"./review-context-Mf-PIgdv.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-C8Rjs7M3.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CJg2LJ8d.js";import{u as o}from"./review-context-B7ASkWtH.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
