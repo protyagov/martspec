c939c03e645c11df9cff47bca945d9328ebcd28c
Andrey <andreybogdanovich06@gmail.com>
2026-06-05 00:15:23 +0300

    Updates

---
+++ development/assets/review-description-kpcd22ej.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B0WwzwYB.js";import{u as i}from"./review-context-Rsf6m41e.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-DD_z9KSz.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B0WwzwYB.js";import{u as o}from"./review-context-Rsf6m41e.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
