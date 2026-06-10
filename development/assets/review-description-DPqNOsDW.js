48035a6aaae299ef8643bdeca5ebd7cfe4791ad4
leyli <tr.leyli@gmail.com>
2026-06-10 21:51:21 +0300

    Updates

--- development/assets/review-description-DPqNOsDW.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BGBWNyYe.js";import{u as i}from"./review-context-1BJan-4U.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
