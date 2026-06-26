240ea0e661d7b448d0ad808ef128a61f9a55fe8e
EvgenKh <hamitcevich@gmail.com>
2026-06-26 20:16:38 +0300

    Updates

---
+++ development/assets/review-description-BL7_uRzi.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DoFE9f3J.js";import{u as i}from"./review-context-HKYY_qrx.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-DTMm0HgF.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dlt-st6B.js";import{u as o}from"./review-context-DGWUG5fx.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
