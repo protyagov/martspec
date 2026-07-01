3fc93ef891386a0faf71559cc98ae268180336f2
leyli <tr.leyli@gmail.com>
2026-07-01 23:14:34 +0300

    MS-1672: prototype added

--- src/atomic/page/scam-protection.tsx
+++ src/atomic/page/scam-protection.tsx
@@ -11,6 +11,8 @@ import CardTitleTextButton from "@/atomic/molecule/card-title-text-button";
 import { Breadcrumb } from "@/atomic/organism/breadcrumb";
 import { useBreadcrumbs } from "@/hooks/useBreadcrumbs"
 import ButtonLinkStyleAnimated from "@/atomic/atom/buttonLinkStyleAnimated";
+import CardTitleTextImageCustom from "@/atomic/molecule/card-title-text-image-custom";
+import BulletList from "@/atomic/molecule/bullet-list";
 
 export default function ScamProtection() {
     const items = useBreadcrumbs();
@@ -19,20 +21,92 @@ export default function ScamProtection() {
         <>
             <NavigationBar />
 
-            <div className="container-xl fs-3 pt-lg-5">
+            <div className="scam-protection">
                 <div className="row mb-3">
                     <Breadcrumb items={items} />
                 </div>
 
-                <Header
-                    title={_("SCAM_PROTECTION.HEAD")}
-                    imgSrc="/img/page/scam-protection/safety1.webp"
-                    imgAlt={_("SCAM_PROTECTION.ALT")}
-                    imgH={398}
-                    imgW={542}
-                >
-                    <p className="font-normal mb-0">{_("SCAM_PROTECTION.DESC")}</p>
-                </Header>
+                <section className="mt-0 row">
+                    <div  className="col-lg-7">
+                        <h1 className="font-normal">{_("SCAM_PROTECTION.HEAD")}</h1>
+                        <p className="font-normal">{_("SCAM_PROTECTION.DESC")}</p>
+                    </div>
+                    <div className="col-lg-5 d-flex p-0 justify-content-center">
+                        <ImageI18N
+                            src="/img/page/scam-protection/safety1.webp"
+                            w={540}
+                            h={300}
+                            cls="p-0 img-fluid"
+                            alt={_("SCAM_PROTECTION.ALT")}
+                        />
+                    </div>
+                </section>
+
+                <section>
+                    <div className="row block g-4 shadow-none" style={{ overflow: "hidden" }}>
+                        <div className="col-lg-6 p-3">
+                            <div className="h-100 rounded-5 p-4" style={{ backgroundColor: "#FFE9E9" }}>
+                                
+                                <div className="d-flex align-items-start">
+                                    <div>
+                                        <img className="p-0" src="/img/page/scam-protection/red-shield.svg" alt="Shield" />
+                                    </div>
+                                    <div className="flex-grow-1">
+                                        <h3 className="m-0 pt-2 ps-3"> {_("CAREERS.HEAD4")}</h3>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-end justify-content-between">
+                                    <div className="col-lg-8 py-4 p-0">
+                                        <BulletList items={[_("ZINC.LIST3.LI1_HEAD")]} iconColor="#FEB3B1" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI2_HEAD")]} iconColor="#FEB3B1" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI3_HEAD")]} iconColor="#FEB3B1" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI4_HEAD")]} iconColor="#FEB3B1" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                    </div>
+                                    <div className="col-lg-4 p-0 text-end">
+                                            <ImageI18N
+                                        src="/img/page/scam-protection/wrong.webp"
+                                        w={135}
+                                        h={160}
+                                        cls="p-0 img-fluid"
+                                        alt={_("CAREERS.ALT2")}
+                                    />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 p-3">
+                            <div className="h-100 rounded-5 p-4" style={{ backgroundColor: "#E8FAE0" }}>
+                                
+                                <div className="d-flex align-items-start">
+                                    <div>
+                                        <img className="p-0" src="/img/page/scam-protection/green-shield.svg" alt="Shield" />
+                                    </div>
+                                    <div className="flex-grow-1">
+                                        <h3 className="m-0 pt-2 ps-3"> {_("CAREERS.HEAD4")}</h3>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-end justify-content-between">
+                                    <div className="col-lg-8 py-4 p-0">
+                                        <BulletList items={[_("ZINC.LIST3.LI1_HEAD")]} iconColor="#9CCA77" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI2_HEAD")]} iconColor="#9CCA77" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI3_HEAD")]} iconColor="#9CCA77" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                        <BulletList items={[_("ZINC.LIST3.LI4_HEAD")]} iconColor="#9CCA77" className="col-bullet-list fw-bold text-muted m-0 p-0" />
+                                    </div>
+                                    <div className="col-lg-4 p-0 text-end">
+                                            <ImageI18N
+                                        src="/img/page/scam-protection/right.webp"
+                                        w={135}
+                                        h={160}
+                                        cls="p-0 img-fluid"
+                                        alt={_("CAREERS.ALT2")}
+                                    />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
 
             </div>
 
