a6c61b67eed4908a771a385aadca8bed0b674ded
Nataliia <danchnataly@gmail.com>
2026-07-14 16:06:47 +0300

    Updates

---
+++ development/assets/review-description-rH_oUFmv.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dtc6uKmw.js";import{u as i}from"./review-context-Bwty_9Z3.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-BorrBNz6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dtc6uKmw.js";import{u as o}from"./review-context-Bwty_9Z3.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
