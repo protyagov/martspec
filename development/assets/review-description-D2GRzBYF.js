d28bc395cd468b0e29d09dfa2b7c5e1613a66255
Ayan Baryshev <ayanbaryshev02@gmail.com>
2026-07-23 09:08:11 +0500

    Updates

---
+++ development/assets/review-description-D2GRzBYF.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Di0OC8QC.js";import{u as i}from"./review-context-Dmb2NYT_.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-PXPZzPh_.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Di0OC8QC.js";import{u as o}from"./review-context-Dmb2NYT_.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
