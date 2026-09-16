be252722707625b721960c68c7a69742a75a9c49
Ir4D <iradedova@gmail.com>
2026-09-16 17:14:48 +0300

    Updates

--- development/assets/review-description-DZ8YtaTc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CozvdXhS.js";import{u as i}from"./review-context-DCwRMdB6.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
--- development/assets/review-head-D6nllrpa.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CozvdXhS.js";import{u as o}from"./review-context-DCwRMdB6.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
