9f47b73c900078a85d5133b7c6336c94cf954b99
Ir4D <iradedova@gmail.com>
2026-09-18 14:57:38 +0300

    Updates

---
+++ development/assets/review-description-29HINnhj.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BI7ei3LC.js";import{u as i}from"./review-context-CXcy1wdc.js";function o(){const{text:e}=i();return t.jsx("p",{className:"review__description",children:e.description})}export{o as R};
---
+++ development/assets/review-head-C6x_GfyU.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BI7ei3LC.js";import{u as o}from"./review-context-CXcy1wdc.js";function i(){const{text:e}=o();return t.jsx("h2",{children:e.head})}export{i as R};
